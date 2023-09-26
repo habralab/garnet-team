47527f0e85db3f9dc67851a2251f72372ff717a7
gfg7 <nakonechnaya040702@gmail.com>
2023-09-27 01:28:28 +0500

    #8 add entity

---
+++ features/team/server/Garnet.Teams.Application/Team.cs
@@ -0,0 +1,10 @@
+namespace Garnet.Teams.Application
+{
+    public record Team(
+        string Id,
+        string Name,
+        string Description,
+        string OwnerUserId,
+        string[] ParticipantUserIds
+    );
+}
