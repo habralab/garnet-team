b252a169e8ff9cc69229691b60999382d029ae01
gfg7 <nakonechnaya040702@gmail.com>
2023-09-28 02:06:23 +0500

    #8 test steps

--- features/team/server/Garnet.Teams.AcceptanceTests/Features/CreateTeam/CreateTeamSteps.cs
+++ features/team/server/Garnet.Teams.AcceptanceTests/Features/CreateTeam/CreateTeamSteps.cs
@@ -1,7 +1,11 @@
+using FluentAssertions;
 using Garnet.Common.AcceptanceTests.Fakes;
 using Garnet.Common.AcceptanceTests.Support;
 using Garnet.Common.Infrastructure.Support;
 using Garnet.Teams.AcceptanceTests.Features.Support;
+using Garnet.Teams.Infrastructure.Api;
+using Garnet.Teams.Infrastructure.Api.TeamCreate;
+using MongoDB.Driver;
 
 namespace Garnet.Teams.AcceptanceTests.Features.CreateTeam
 {
@@ -20,31 +24,37 @@ namespace Garnet.Teams.AcceptanceTests.Features.CreateTeam
         public async Task GivenСуществуетПользователь(string username)
         {
             var id = _currentUserProviderFake.RegisterUser(username, Uuid.NewMongo());
-            _user = GiveMe.User().WithId(id).WithUserName(username);
+            _user = GiveMe.User().WithId(id);
         }
 
         [When(@"пользователь '([^']*)' создает команду '([^']*)'")]
         public async Task WhenПользовательСоздаетКоманду(string username, string team)
         {
-
+            var input = new TeamCreateInput(_user.Id, team, string.Empty);
+            await Mutation.CreateTeam(CancellationToken.None, input);
         }
 
         [Then(@"в системе присутствует команда '([^']*)'")]
         public async Task ThenВСистемеПрисутствуетКоманда(string team)
         {
-
+            var newTeam = await Db.Teams.Find(x => x.Name == team).FirstOrDefaultAsync();
+            newTeam.Should().NotBeNull();
         }
 
         [Then(@"пользователь '([^']*)' является владельцем команды '([^']*)'")]
         public async Task ThenПользовательЯвляетсяВладельцемКоманды(string username, string team)
         {
-
+            var newTeam = await Db.Teams.Find(x => x.Name == team).FirstOrDefaultAsync();
+            newTeam.OwnerUserId.Should().Be(_user.Id);
         }
 
         [Then(@"пользователь '([^']*)' является участником команды '([^']*)'")]
         public async Task ThenПользовательЯвляетсяУчастникомКоманды(string username, string team)
         {
-
+            var newTeam = await Db.Teams.Find(x => x.Name == team).FirstOrDefaultAsync();
+            var participants = await Db.TeamParticipants.Find(x => x.TeamId == newTeam.Id).ToListAsync();
+            participants.Count.Should().Be(1);
+            participants.First().UserId.Should().Be(_user.Id);
         }
     }
 }
