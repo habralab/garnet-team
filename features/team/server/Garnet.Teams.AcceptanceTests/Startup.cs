e3b6e5b740fbd249654ae63492075bf425341755
gfg7 <nakonechnaya040702@gmail.com>
2023-09-28 02:05:59 +0500

    #8 set di

--- features/team/server/Garnet.Teams.AcceptanceTests/Startup.cs
+++ features/team/server/Garnet.Teams.AcceptanceTests/Startup.cs
@@ -1,4 +1,13 @@
+using Garnet.Common.AcceptanceTests.Fakes;
+using Garnet.Common.AcceptanceTests.Support;
+using Garnet.Common.Application;
+using Garnet.Common.Infrastructure.Migrations;
+using Garnet.Teams.Application;
+using Garnet.Teams.Infrastructure.Api;
+using Garnet.Teams.Infrastructure.MongoDb;
+using Garnet.Teams.Infrastructure.MongoDb.Migration;
 using Microsoft.Extensions.DependencyInjection;
+using Mongo2Go;
 using SolidToken.SpecFlow.DependencyInjection;
 
 namespace Garnet.Teams.AcceptanceTests
@@ -9,8 +18,36 @@ namespace Garnet.Teams.AcceptanceTests
         public static IServiceCollection CreateServices()
         {
             var services = new ServiceCollection();
+            services.AddScoped<CurrentUserProviderFake>();
+            services.AddScoped<ICurrentUserProvider>(o => o.GetRequiredService<CurrentUserProviderFake>());
+
+            services.AddScoped<ITeamParticipantRepository, TeamParticipantRepository>();
+            services.AddScoped<ITeamRepository, TeamRepository>();
+            
+            services.AddScoped<TeamService>();
+
+            services.AddScoped<TeamsMutation>();
+
+            services.AddScoped<GiveMe>();
+            services.AddScoped<StepsArgs>();
+
+            AddMongoDb(services);
 
             return services;
         }
+
+        private static void AddMongoDb(IServiceCollection services)
+    {
+        services.AddScoped<MongoDbRunner>(_ => MongoDbRunner.Start());
+        services.AddScoped<DbFactory>(o =>
+        {
+            var mongo = o.GetRequiredService<MongoDbRunner>();
+            return new DbFactory(mongo.ConnectionString);
+        });
+        services.AddScoped<Db>(o => o.GetRequiredService<DbFactory>().Create());
+
+        services.AddScoped<IRepeatableMigration, CreateIndexesTeamMigration>();
+        services.AddScoped<IRepeatableMigration, CreateIndexesTeamParticipantMigration>();
+    }
     }
 }
---
+++ features/team/server/Garnet.Teams/Startup.cs
@@ -0,0 +1,37 @@
+using Garnet.Common.Infrastructure.Migrations;
+using Garnet.Teams.Application;
+using Garnet.Teams.Infrastructure.Api;
+using Garnet.Teams.Infrastructure.MongoDb;
+using Garnet.Teams.Infrastructure.MongoDb.Migration;
+using HotChocolate.Execution.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Garnet.Team
+{
+    public static class Startup
+    {
+        public static void AddGarnetUsers(this IRequestExecutorBuilder builder)
+        {
+            builder.AddType<TeamsMutation>();
+            builder.Services.AddGarnetTeamsInternal();
+            builder.Services.AddRepeatableMigrations();
+        }
+
+        private static void AddGarnetTeamsInternal(this IServiceCollection services)
+        {
+            const string mongoConnStringEnv = "MONGO_CONNSTRING";
+            var mongoDbConnString =
+                Environment.GetEnvironmentVariable(mongoConnStringEnv)
+                ?? throw new Exception($"No {mongoConnStringEnv} environment variable was provided.");
+            services.AddScoped<DbFactory>(o => new DbFactory(mongoDbConnString));
+            services.AddScoped<TeamService>();
+            services.AddScoped<ITeamRepository, TeamRepository>();
+            services.AddScoped<ITeamParticipantRepository, TeamParticipantRepository>();
+        }
+        private static void AddRepeatableMigrations(this IServiceCollection services)
+        {
+            services.AddScoped<IRepeatableMigration, CreateIndexesTeamMigration>();
+            services.AddScoped<IRepeatableMigration, CreateIndexesTeamParticipantMigration>();
+        }
+    }
+}
