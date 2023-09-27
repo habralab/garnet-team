035ef62ebd52b425b838087e0fccd3f67be186b9
Demorey <demorey2@gmail.com>
2023-09-27 16:48:32 +0300

    init AcceptanceTests project

---
+++ features/project/server/Garnet.Projects.AcceptanceTests/ScenarioDecorator.cs
@@ -0,0 +1,25 @@
+using Garnet.Common.Infrastructure.Migrations;
+using Microsoft.Extensions.DependencyInjection;
+using Mongo2Go;
+using TechTalk.SpecFlow;
+
+namespace Garnet.Projects.AcceptanceTests;
+
+[Binding]
+public class ScenarioDecorator
+{
+    [BeforeScenario]
+    public async Task BeforeScenario(ScenarioContext context)
+    {
+        var services = context.ScenarioContainer.Resolve<IServiceProvider>();
+        await services.ExecuteRepeatableMigrations(CancellationToken.None);
+    }
+    
+    [AfterScenario]
+    public void AfterScenario(ScenarioContext context)
+    {
+        var services = context.ScenarioContainer.Resolve<IServiceProvider>();
+        var mongo = services.GetRequiredService<MongoDbRunner>();
+        mongo.Dispose();
+    }
+}
