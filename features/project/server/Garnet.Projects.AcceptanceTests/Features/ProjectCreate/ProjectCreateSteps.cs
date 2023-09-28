384d3eba57d624b119d2743715bc45ececc2c0cf
Demorey <demorey2@gmail.com>
2023-09-28 22:34:32 +0300

    #18 added test steps

--- features/project/server/Garnet.Projects.AcceptanceTests/Features/ProjectCreate/ProjectCreateSteps.cs
+++ features/project/server/Garnet.Projects.AcceptanceTests/Features/ProjectCreate/ProjectCreateSteps.cs
@@ -4,6 +4,7 @@ using Garnet.Common.AcceptanceTests.Support;
 using Garnet.Common.Infrastructure.Support;
 using Garnet.Projects.AcceptanceTests;
 using Garnet.Projects.AcceptanceTests.Support;
+using Garnet.Projects.Infrastructure.Api.ProjectCreate;
 using Garnet.Projects.Infrastructure.MongoDb;
 using MongoDB.Driver;
 using TechTalk.SpecFlow;
@@ -23,25 +24,31 @@ public class ProjectCreateSteps : BaseSteps
     [Given(@"существует пользователь '([^']*)'")]
     public async Task GivenСуществуетПользователь(string username)
     {
-        
+        var id = _currentUserProviderFake.RegisterUser(username, Uuid.NewMongo());
+        var user = GiveMe.User().WithId(id);
     }
 
     [When(@"пользователь '(.*)' создает проект '(.*)'")]
     public async Task WhenПользовательСоздаетПроект(string username, string projectName)
     {
-        
+        await Mutation.ProjectCreate(
+            CancellationToken.None,
+            _currentUserProviderFake.LoginAs(username),
+            new ProjectCreateInput(projectName));
     }
 
     [Then(@"в системе существует проект '(.*)'")]
     public async Task ThenСуществуетПроектСНазванием(string projectName)
     {
-
+        var project = await Db.Projects.Find(o => o.ProjectName == projectName).FirstAsync();
+        project.ProjectName.Should().Be(projectName);
     }
 
     [Then(@"пользователь '(.*)' является владельцем проекта '(.*)'")]
     public async Task ThenПользовательЯвляетсяВладельцемПроекта(string username, string projectName)
     {
-
+        var project = await Db.Projects.Find(o => o.ProjectName == projectName).FirstAsync();
+        _currentUserProviderFake.GetUserName(project.OwnerUserId).Should().Be(username);
     }
 
 }
