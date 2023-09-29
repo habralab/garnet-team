5bcc0f26cd8e022b4905ccd90a73e7bcbfab434a
Demorey <demorey2@gmail.com>
2023-09-29 15:42:42 +0300

    #19 add test steps

---
+++ features/project/server/Garnet.Projects.AcceptanceTests/Features/ProjectGet/ProjectGetSteps.cs
@@ -0,0 +1,45 @@
+﻿using FluentAssertions;
+using Garnet.Common.AcceptanceTests.Fakes;
+using Garnet.Common.Infrastructure.Support;
+using Garnet.Projects.AcceptanceTests.Support;
+using MongoDB.Driver;
+using TechTalk.SpecFlow;
+
+namespace Garnet.Projects.AcceptanceTests.Features.ProjectGet;
+
+[Binding]
+public class ProjectGetSteps : BaseSteps
+{
+    private readonly CurrentUserProviderFake _currentUserProviderFake;
+
+    public ProjectGetSteps(CurrentUserProviderFake currentUserProviderFake, StepsArgs args) : base(args)
+    {
+        _currentUserProviderFake = currentUserProviderFake;
+    }
+
+    [Given(@"существует пользователь '([^']*)'")]
+    public Task GivenСуществуетПользователь(string username)
+    {
+        _currentUserProviderFake.RegisterUser(username, Uuid.NewMongo());
+        return Task.CompletedTask;
+    }
+
+    [Given(@"существует проект '(.*)' с описанием '(.*)'")]
+    public async Task ThenСуществуетПроектСНазваниемИОписанием(string projectName, string description)
+    {
+        var project = GiveMe.Project().WithProjectName(projectName).WithDescription(description);
+        await Db.Projects.InsertOneAsync(project);
+    }
+
+    [When(@"пользователь '(.*)' просматривает карточку проекта '(.*)'")]
+    public async Task WhenПользовательПросматриваетПроект(string username, string projectName)
+    {
+
+    }
+
+    [Then(@"описание проекта состоит из '(.*)'")]
+    public async Task ThenОписаниеПроектаСостоитИз(string description)
+    {
+
+    }
+}
