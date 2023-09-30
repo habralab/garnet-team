b76a7c7839b8a8aa1146b5746f9d9afd6b66e3b2
gfg7 <nakonechnaya040702@gmail.com>
2023-09-30 19:43:56 +0500

    #11 add with given owner step

--- features/team/server/Garnet.Teams.AcceptanceTests/Features/TeamDelete/TeamDeleteSteps.cs
+++ features/team/server/Garnet.Teams.AcceptanceTests/Features/TeamDelete/TeamDeleteSteps.cs
@@ -7,6 +7,12 @@ namespace Garnet.Teams.AcceptanceTests.Features.TeamDelete
         {
         }
 
+        [Given(@"владельцем команды '([^']*)' является '([^']*)'")]
+        public Task GivenВладелецемКомандыЯвляется(string username, string team)
+        {
+            return Task.CompletedTask;
+        }
+
         [When(@"'([^']*)' удаляет команду '([^']*)'")]
         public Task WhenУдаляетКоманду(string username, string team)
         {
@@ -15,6 +21,12 @@ namespace Garnet.Teams.AcceptanceTests.Features.TeamDelete
 
         [Then(@"команды '([^']*)' в системе не существует")]
         public Task ThenКомандыВСистемеНеСуществует(string team)
+        {
+            return Task.CompletedTask;
+        }
+
+        [Then(@"пользователь получает ошибку '([^']*)'")]
+        public Task ThenПользовательПолучаетОшибку(string errorMsg)
         {
             return Task.CompletedTask;
         }
