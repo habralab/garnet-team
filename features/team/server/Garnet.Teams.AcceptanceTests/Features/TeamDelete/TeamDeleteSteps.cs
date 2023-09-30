8bf7125fa860b0b8fe6a32313c5fdd186926e27d
gfg7 <nakonechnaya040702@gmail.com>
2023-09-30 20:11:12 +0500

    #11 add team delete mutation to step
    (cherry picked from commit cd92cf1942e0eda54f79db2b00d7014239754bd2)

--- features/team/server/Garnet.Teams.AcceptanceTests/Features/TeamDelete/TeamDeleteSteps.cs
+++ features/team/server/Garnet.Teams.AcceptanceTests/Features/TeamDelete/TeamDeleteSteps.cs
@@ -1,33 +1,62 @@
+using FluentAssertions;
+using Garnet.Common.AcceptanceTests.Fakes;
+using Garnet.Teams.Infrastructure.Api.TeamDelete;
+using Garnet.Teams.Infrastructure.MongoDb;
+using MongoDB.Driver;
+
 namespace Garnet.Teams.AcceptanceTests.Features.TeamDelete
 {
     [Binding]
     public class TeamDeleteSteps : BaseSteps
     {
-        public TeamDeleteSteps(StepsArgs args) : base(args)
+        private readonly FilterDefinitionBuilder<TeamDocument> _f = Builders<TeamDocument>.Filter;
+        private readonly UpdateDefinitionBuilder<TeamDocument> _u = Builders<TeamDocument>.Update;
+        private readonly CurrentUserProviderFake _currentUserProviderFake;
+        private TeamDeletePayload _result = null!;
+        private Exception _exception = null!;
+
+        public TeamDeleteSteps(CurrentUserProviderFake currentUserProviderFake, StepsArgs args) : base(args)
         {
+            _currentUserProviderFake = currentUserProviderFake;
         }
 
         [Given(@"владельцем команды '([^']*)' является '([^']*)'")]
-        public Task GivenВладелецемКомандыЯвляется(string username, string team)
+        public async Task GivenВладелецемКомандыЯвляется(string teamName, string username)
         {
-            return Task.CompletedTask;
+            _currentUserProviderFake.LoginAs(username);
+            await Db.Teams.FindOneAndUpdateAsync(
+                _f.Eq(x => x.Name, teamName),
+                _u.Set(o => o.OwnerUserId, _currentUserProviderFake.UserId)
+            );
         }
 
         [When(@"'([^']*)' удаляет команду '([^']*)'")]
-        public Task WhenУдаляетКоманду(string username, string team)
+        public async Task WhenУдаляетКоманду(string username, string teamName)
         {
-            return Task.CompletedTask;
+            var claims = _currentUserProviderFake.LoginAs(username);
+            var team = await Db.Teams.Find(x => x.Name == teamName).FirstAsync();
+
+            try
+            {
+                _result = await Mutation.TeamDelete(CancellationToken.None, claims, team.Id);
+            }
+            catch (Exception ex)
+            {
+                _exception = ex;
+            }
         }
 
         [Then(@"команды '([^']*)' в системе не существует")]
-        public Task ThenКомандыВСистемеНеСуществует(string team)
+        public async Task ThenКомандыВСистемеНеСуществует(string teamName)
         {
-            return Task.CompletedTask;
+            var team = await Db.Teams.Find(x => x.Name == teamName).FirstOrDefaultAsync();
+            team.Should().BeNull();
         }
 
         [Then(@"пользователь получает ошибку '([^']*)'")]
         public Task ThenПользовательПолучаетОшибку(string errorMsg)
         {
+            _exception.Message.Should().Be(errorMsg);
             return Task.CompletedTask;
         }
     }
