f7c8ad30a0549d1e284b28b9a11c333caa0bb40a
gfg7 <nakonechnaya040702@gmail.com>
2023-09-30 15:25:06 +0500

    #13 add teams filter query

---
+++ features/team/server/Garnet.Teams.Infrastructure/Api/TeamsFilter/TeamsFilterPayload.cs
@@ -0,0 +1,6 @@
+using Garnet.Teams.Infrastructure.Api.TeamGet;
+
+namespace Garnet.Teams.Infrastructure.Api.TeamsFilter
+{
+    public record TeamsFilterPayload(TeamPayload[] Teams);
+}
--- features/team/server/Garnet.Teams.Infrastructure/Api/TeamsQuery.cs
+++ features/team/server/Garnet.Teams.Infrastructure/Api/TeamsQuery.cs
@@ -1,6 +1,7 @@
 using System.Security.Claims;
 using Garnet.Teams.Application;
 using Garnet.Teams.Infrastructure.Api.TeamGet;
+using Garnet.Teams.Infrastructure.Api.TeamsFilter;
 using HotChocolate.Execution;
 using HotChocolate.Types;
 
@@ -22,6 +23,11 @@ namespace Garnet.Teams.Infrastructure.Api
             var team = await _teamService.GetTeamById(ct, teamId)
                 ?? throw new QueryException($"Команда с идентификатором '{teamId}' не найдена");
             return new TeamPayload(team.Id, team.Name, team.Description, team.Tags);
+        }
+
+        public async Task<TeamsFilterPayload> TeamsFilter(CancellationToken ct, TeamsFilterInput input)
+        {
+            return null;
         }
     }
 }
