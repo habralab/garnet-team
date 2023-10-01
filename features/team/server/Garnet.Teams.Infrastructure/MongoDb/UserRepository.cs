3b94d842eb59244d1d6bcf1cd549b5ba5ded6c74
gfg7 <nakonechnaya040702@gmail.com>
2023-10-02 01:43:09 +0500

    #12 set user repository

--- features/team/server/Garnet.Teams.Infrastructure/MongoDb/UserRepository.cs
+++ features/team/server/Garnet.Teams.Infrastructure/MongoDb/UserRepository.cs
@@ -1,17 +1,33 @@
 using Garnet.Teams.Application;
+using MongoDB.Driver;
 
 namespace Garnet.Teams.Infrastructure.MongoDb
 {
     public class UserRepository : IUserRepository
     {
-        public Task<string> AddUser(CancellationToken ct, string userId)
+        private readonly DbFactory _dbFactory;
+        private readonly FilterDefinitionBuilder<string> _f = Builders<string>.Filter;
+
+        public UserRepository(DbFactory dbFactory)
         {
-            throw new NotImplementedException();
+            _dbFactory = dbFactory;
         }
 
-        public Task<string?> GetUser(CancellationToken ct, string userId)
+        public async Task<string> AddUser(CancellationToken ct, string userId)
         {
-            throw new NotImplementedException();
+            var db = _dbFactory.Create();
+            await db.Users.InsertOneAsync(
+                userId,
+                cancellationToken: ct
+            );
+
+            return userId;
+        }
+
+        public async Task<string?> GetUser(CancellationToken ct, string userId)
+        {
+            var db = _dbFactory.Create();
+            return await db.Users.Find(x => x == userId).FirstOrDefaultAsync(ct);
         }
     }
 }
