268dbbd79f1fda3d18db3650088aa2a836e85bb4
gfg7 <nakonechnaya040702@gmail.com>
2023-10-01 14:35:48 +0500

    #10 init test proj

---
+++ features/team/server/Garnet.Teams.AcceptanceTests/Features/TeamEdit/TeamEditSteps.cs
@@ -0,0 +1,11 @@
+namespace Garnet.Teams.AcceptanceTests.Features.TeamEdit
+{
+    [Binding]
+    public class TeamEditSteps : BaseSteps
+    {
+        public TeamEditSteps(StepsArgs args) : base(args)
+        {
+        }
+
+    }
+}
