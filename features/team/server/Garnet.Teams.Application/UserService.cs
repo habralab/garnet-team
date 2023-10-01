e59aae0198b5561b2654efa2a8914355d4c874a4
gfg7 <nakonechnaya040702@gmail.com>
2023-10-02 01:34:13 +0500

    #12 add user service

---
+++ features/team/server/Garnet.Teams.Application/UserService.cs
@@ -0,0 +1,15 @@
+namespace Garnet.Teams.Application
+{
+    public class UserService
+    {
+        public Task<string> AddUser(CancellationToken ct, string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string?> GetUser(CancellationToken ct, string userId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- features/team/server/Garnet.Teams.Infrastructure/Consumers/UserCreatedEventConsumer.cs
+++ features/team/server/Garnet.Teams.Infrastructure/Consumers/UserCreatedEventConsumer.cs
@@ -1,13 +1,20 @@
 using Garnet.Common.Application.MessageBus;
+using Garnet.Teams.Application;
 using Garnet.Users.Events;
 
 namespace Garnet.Teams.Infrastructure.Consumers
 {
     public class UserCreatedEventConsumer : IMessageBusConsumer<UserCreatedEvent>
     {
-        public Task Consume(UserCreatedEvent message)
+        private readonly UserService _userService;
+        public UserCreatedEventConsumer(UserService userService)
         {
-            throw new NotImplementedException();
+            _userService = userService;
+        }
+
+        public async Task Consume(UserCreatedEvent message)
+        {
+            await _userService.AddUser(CancellationToken.None, message.UserId);
         }
     }
 }
