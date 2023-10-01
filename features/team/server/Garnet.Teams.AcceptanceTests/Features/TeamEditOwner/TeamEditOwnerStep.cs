560567ef2543fcae2f3956cba2f95ffd1c6ed52e
gfg7 <nakonechnaya040702@gmail.com>
2023-10-01 21:29:07 +0500

    #12 set steps

--- features/team/server/Garnet.Teams.AcceptanceTests/Features/TeamEditOwner/TeamEditOwnerStep.cs
+++ features/team/server/Garnet.Teams.AcceptanceTests/Features/TeamEditOwner/TeamEditOwnerStep.cs
@@ -6,5 +6,11 @@ namespace Garnet.Teams.AcceptanceTests.Features.TeamEditOwner
         public TeamEditOwnerStep(StepsArgs args) : base(args)
         {
         }
+
+        [When(@"'([^']*)' изменяет владельца команды '([^']*)' на пользователя '([^']*)'")]
+        public Task WhenИзменяетВладельцаКомандыНаПользователя(string username, string teamName, string newOwnerUsername)
+        {
+            return Task.CompletedTask;
+        }
     }
 }
