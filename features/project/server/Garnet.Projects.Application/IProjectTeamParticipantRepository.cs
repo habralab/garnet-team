320fa7f832795fcbe4a732a0398c72a3096dc5c4
Demorey <demorey2@gmail.com>
2023-10-05 00:53:10 +0300

    #51 add methods to app

---
+++ features/project/server/Garnet.Projects.Application/IProjectTeamParticipantRepository.cs
@@ -0,0 +1,7 @@
+﻿namespace Garnet.Projects.Application;
+
+public interface IProjectTeamParticipantRepository
+{
+    Task<ProjectTeamParticipant> AddProjectTeamParticipant(CancellationToken ct, string id, string teamId, string projectId);
+    Task<ProjectTeamParticipant[]> GetProjectTeamParticipantByProjectId(CancellationToken ct, string projectId);
+}
---
+++ features/project/server/Garnet.Projects.Application/ProjectTeamParticipantService.cs
@@ -0,0 +1,22 @@
+﻿using FluentResults;
+
+namespace Garnet.Projects.Application;
+
+public class ProjectTeamParticipantService
+{
+    private readonly IProjectTeamParticipantRepository _repository;
+
+    public ProjectTeamParticipantService(IProjectTeamParticipantRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task<Result<ProjectTeamParticipant>> AddProjectTeamParticipant(CancellationToken ct, string id, string teamId, string projectId)
+    {
+        return await _repository.AddProjectTeamParticipant(ct, id, teamId, projectId);
+    }
+    public async Task<Result<ProjectTeamParticipant[]>> GetProjectTeamParticipantByProjectId(CancellationToken ct, string teamId)
+    {
+        return await _repository.GetProjectTeamParticipantByProjectId(ct, teamId);
+    }
+}
