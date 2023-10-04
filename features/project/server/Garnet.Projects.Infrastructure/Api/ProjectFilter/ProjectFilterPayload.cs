3a4bac47083a2e4aa7555f184596df3b251edfa2
Demorey <demorey2@gmail.com>
2023-10-04 15:22:05 +0300

    #23 add Tags field to ProjectPayload

---
+++ features/project/server/Garnet.Projects.Infrastructure/Api/ProjectFilter/ProjectFilterPayload.cs
@@ -0,0 +1,5 @@
+﻿using Garnet.Projects.Infrastructure.Api.ProjectGet;
+
+namespace Garnet.Projects.Infrastructure.Api.ProjectFilter;
+
+public record ProjectFilterPayload(ProjectPayload[] Projects);
