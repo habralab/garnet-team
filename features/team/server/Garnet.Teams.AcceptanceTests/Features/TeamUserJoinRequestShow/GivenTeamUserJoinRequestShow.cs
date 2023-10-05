6362259caa7051aff0bd0a2f3b0b8c28cb441fe6
gfg7 <nakonechnaya040702@gmail.com>
2023-10-06 02:02:22 +0500

    #15 set steps

---
+++ features/team/server/Garnet.Teams.AcceptanceTests/Features/TeamUserJoinRequestShow/GivenTeamUserJoinRequestShow.cs
@@ -0,0 +1,9 @@
+namespace Garnet.Teams.AcceptanceTests.Features.TeamUserJoinRequestShow
+{
+    public class GivenTeamUserJoinRequestShow
+    {
+        public string TeamName { get; set; } = null!;
+        public string TeamOwner { get; set; } = null!;
+        public string RequestedToJoinUser { get; set; } = null!;
+    }
+}
--- features/team/server/Garnet.Teams.AcceptanceTests/Features/TeamUserJoinRequestShow/TeamUserJoinRequestShowSteps.cs
+++ features/team/server/Garnet.Teams.AcceptanceTests/Features/TeamUserJoinRequestShow/TeamUserJoinRequestShowSteps.cs
@@ -1,10 +1,54 @@
+using Garnet.Common.AcceptanceTests.Fakes;
+using Garnet.Common.Infrastructure.Support;
+using Garnet.Teams.AcceptanceTests.Support;
+using Garnet.Teams.Infrastructure.MongoDb;
+using TechTalk.SpecFlow.Assist;
+
 namespace Garnet.Teams.AcceptanceTests.Features.TeamUserJoinRequestShow
 {
     [Binding]
     public class TeamUserJoinRequestShowSteps : BaseSteps
     {
-        public TeamUserJoinRequestShowSteps(StepsArgs args) : base(args)
+        private readonly CurrentUserProviderFake _currentUserProviderFake;
+
+        public TeamUserJoinRequestShowSteps(CurrentUserProviderFake currentUserProviderFake, StepsArgs args) : base(args)
         {
+            _currentUserProviderFake = currentUserProviderFake;
+        }
+
+        [Given(@"существуют пользователи и команда со следующими параметрами")]
+        public async Task GivenСуществуютПользователиИКомандаСоСледующимиПараметрами(Table table)
+        {
+            var given = table.CreateInstance<GivenTeamUserJoinRequestShow>();
+
+            var teamOwner = TeamUserDocument.Create(Uuid.NewMongo(), given.TeamOwner);
+            await Db.TeamUsers.InsertOneAsync(teamOwner);
+            _currentUserProviderFake.RegisterUser(teamOwner.Username, teamOwner.Id);
+
+            var teamId = Uuid.NewMongo();
+            var team = GiveMe.Team()
+                .WithId(teamId)
+                .WithName(given.TeamName)
+                .WithOwnerUserId(teamOwner.Id);
+            await Db.Teams.InsertOneAsync(team);
+
+            var requestedToJoinUser = TeamUserDocument.Create(Uuid.NewMongo(), given.RequestedToJoinUser);
+            await Db.TeamUsers.InsertOneAsync(requestedToJoinUser);
+
+            var requestToJoin = TeamUserJoinRequestDocument.Create(Uuid.NewMongo(), requestedToJoinUser.Id, teamId);
+            await Db.TeamUserJoinRequest.InsertOneAsync(requestToJoin);
+        }
+
+        [When(@"'(.*)' просматривает заявки на вступление в команду '(.*)'")]
+        public Task WhenПросматриваетЗаявкиНаВступлениеВКоманду(string username, string teamName)
+        {
+            return Task.CompletedTask;
+        }
+
+        [Then(@"в списке отображается '(.*)' заявка")]
+        public Task ThenВСпискеОтображаетсяКоманда(int resultCount)
+        {
+            return Task.CompletedTask;
         }
     }
 }
