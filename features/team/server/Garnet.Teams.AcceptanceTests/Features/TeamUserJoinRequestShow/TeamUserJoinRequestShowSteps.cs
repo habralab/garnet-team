a4175ddce50df918281f96f9c276f6ae1b5f146d
gfg7 <nakonechnaya040702@gmail.com>
2023-10-06 01:28:30 +0500

    #15 init test proj

---
+++ features/team/server/Garnet.Teams.AcceptanceTests/Features/TeamUserJoinRequestShow/TeamUserJoinRequestShowSteps.cs
@@ -0,0 +1,10 @@
+namespace Garnet.Teams.AcceptanceTests.Features.TeamUserJoinRequestShow
+{
+    [Binding]
+    public class TeamUserJoinRequestShowSteps : BaseSteps
+    {
+        public TeamUserJoinRequestShowSteps(StepsArgs args) : base(args)
+        {
+        }
+    }
+}
