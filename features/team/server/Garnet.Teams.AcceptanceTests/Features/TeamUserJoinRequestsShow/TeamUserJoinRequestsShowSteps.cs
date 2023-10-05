0963b11ba925565f7c3c98085cc67df4ae7ca249
gfg7 <nakonechnaya040702@gmail.com>
2023-10-06 02:33:23 +0500

    #15 add query to steps

--- features/team/server/Garnet.Teams.AcceptanceTests/Features/TeamUserJoinRequestsShow/TeamUserJoinRequestsShowSteps.cs
+++ features/team/server/Garnet.Teams.AcceptanceTests/Features/TeamUserJoinRequestsShow/TeamUserJoinRequestsShowSteps.cs
@@ -1,7 +1,10 @@
+using FluentAssertions;
 using Garnet.Common.AcceptanceTests.Fakes;
 using Garnet.Common.Infrastructure.Support;
 using Garnet.Teams.AcceptanceTests.Support;
+using Garnet.Teams.Infrastructure.Api.TeamUserJoinRequestsShow;
 using Garnet.Teams.Infrastructure.MongoDb;
+using MongoDB.Driver;
 using TechTalk.SpecFlow.Assist;
 
 namespace Garnet.Teams.AcceptanceTests.Features.TeamUserJoinRequestShow
@@ -10,6 +13,7 @@ namespace Garnet.Teams.AcceptanceTests.Features.TeamUserJoinRequestShow
     public class TeamUserJoinRequestsShowSteps : BaseSteps
     {
         private readonly CurrentUserProviderFake _currentUserProviderFake;
+        private TeamUserJoinRequestsShowPayload _result = null!;
 
         public TeamUserJoinRequestsShowSteps(CurrentUserProviderFake currentUserProviderFake, StepsArgs args) : base(args)
         {
@@ -40,14 +44,18 @@ namespace Garnet.Teams.AcceptanceTests.Features.TeamUserJoinRequestShow
         }
 
         [When(@"'(.*)' просматривает заявки на вступление в команду '(.*)'")]
-        public Task WhenПросматриваетЗаявкиНаВступлениеВКоманду(string username, string teamName)
+        public async Task WhenПросматриваетЗаявкиНаВступлениеВКоманду(string username, string teamName)
         {
-            return Task.CompletedTask;
+            var claims = _currentUserProviderFake.LoginAs(username);
+            var team = await Db.Teams.Find(x => x.Name == teamName).FirstAsync();
+
+            _result = await Query.TeamUserJoinRequestsShow(CancellationToken.None, claims, team.Id);
         }
 
         [Then(@"в списке отображается '(.*)' заявка")]
         public Task ThenВСпискеОтображаетсяКоманда(int resultCount)
         {
+            _result.TeamUserJoinRequests.Count().Should().Be(resultCount);
             return Task.CompletedTask;
         }
     }
