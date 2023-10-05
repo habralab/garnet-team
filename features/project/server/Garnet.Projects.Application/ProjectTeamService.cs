8fc0d7245766d791bd83dc208c7c24f834d62cda
Demorey <demorey2@gmail.com>
2023-10-05 17:54:43 +0300

    #51 add ProjectTeam methods to app

---
+++ features/project/server/Garnet.Projects.Application/ProjectTeamService.cs
@@ -0,0 +1,25 @@
+﻿using FluentResults;
+
+namespace Garnet.Projects.Application;
+
+public class ProjectTeamService
+{
+    private readonly IProjectTeamRepository _repository;
+
+    public ProjectTeamService(IProjectTeamRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task<ProjectTeam> AddProjectTeam(CancellationToken ct, string teamId,
+        string teamName, string ownerUSerId)
+    {
+        return await _repository.AddProjectTeam(ct, teamId, teamName, ownerUSerId);
+    }
+
+    public async Task<ProjectTeam> UpdateProjectTeam(CancellationToken ct, string teamId,
+        string teamName, string ownerUserId)
+    {
+        return await _repository.UpdateProjectTeam(ct, teamId, teamName, ownerUserId);
+    }
+}
