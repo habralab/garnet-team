421fc4171d3aa111b990b80ca46bcea5f80f49cf
gfg7 <nakonechnaya040702@gmail.com>
2023-10-06 21:13:24 +0500

    #15 fix scenario

--- features/team/server/Garnet.Teams.AcceptanceTests/Features/TeamUserJoinRequestsShow/GivenTeamUserJoinRequestsShow.cs
+++
@@ -1,9 +0,0 @@
-namespace Garnet.Teams.AcceptanceTests.Features.TeamUserJoinRequestShow
-{
-    public class GivenTeamUserJoinRequestsShow
-    {
-        public string TeamName { get; set; } = null!;
-        public string TeamOwner { get; set; } = null!;
-        public string RequestedToJoinUser { get; set; } = null!;
-    }
-}
--- features/team/server/Garnet.Teams.AcceptanceTests/Features/TeamUserJoinRequestsShow/TeamUserJoinRequestsShowSteps.cs
+++ features/team/server/Garnet.Teams.AcceptanceTests/Features/TeamUserJoinRequestsShow/TeamUserJoinRequestsShowSteps.cs
@@ -20,29 +20,6 @@ namespace Garnet.Teams.AcceptanceTests.Features.TeamUserJoinRequestShow
             _currentUserProviderFake = currentUserProviderFake;
         }
 
-        [Given(@"существуют пользователи и команда со следующими параметрами")]
-        public async Task GivenСуществуютПользователиИКомандаСоСледующимиПараметрами(Table table)
-        {
-            var given = table.CreateInstance<GivenTeamUserJoinRequestsShow>();
-
-            var teamOwner = TeamUserDocument.Create(Uuid.NewMongo(), given.TeamOwner);
-            await Db.TeamUsers.InsertOneAsync(teamOwner);
-            _currentUserProviderFake.RegisterUser(teamOwner.Username, teamOwner.Id);
-
-            var teamId = Uuid.NewMongo();
-            var team = GiveMe.Team()
-                .WithId(teamId)
-                .WithName(given.TeamName)
-                .WithOwnerUserId(teamOwner.Id);
-            await Db.Teams.InsertOneAsync(team);
-
-            var requestedToJoinUser = TeamUserDocument.Create(Uuid.NewMongo(), given.RequestedToJoinUser);
-            await Db.TeamUsers.InsertOneAsync(requestedToJoinUser);
-
-            var requestToJoin = TeamUserJoinRequestDocument.Create(Uuid.NewMongo(), requestedToJoinUser.Id, teamId);
-            await Db.TeamUserJoinRequest.InsertOneAsync(requestToJoin);
-        }
-
         [When(@"'(.*)' просматривает заявки на вступление в команду '(.*)'")]
         public async Task WhenПросматриваетЗаявкиНаВступлениеВКоманду(string username, string teamName)
         {
