2a59d2beefa0cfee042d4fac621edbe0a8d6da0b
Demorey <demorey2@gmail.com>
2023-10-06 14:57:09 +0300

    #44 add test steps

---
+++ features/project/server/Garnet.Projects.AcceptanceTests/Features/ProjectTeamJoinRequest/ProjectTeamJoinRequestSteps.cs
@@ -0,0 +1,30 @@
+﻿using MongoDB.Driver;
+using TechTalk.SpecFlow;
+
+namespace Garnet.Projects.AcceptanceTests.Features.ProjectTeamJoinRequest;
+
+[Binding]
+public class ProjectTeamJoinRequestSteps : BaseSteps
+{
+    public ProjectTeamJoinRequestSteps(StepsArgs args) : base(args)
+    {
+    }
+
+    [Given(@"пользователь '([^']*)' является владельцем команды '([^']*)'")]
+    public async Task GivenПользовательЯвляетсяВладельцемКоманды(string username, string teamName)
+    {
+
+    }
+
+    [When(@"пользователь '([^']*)' отправляет заявку на вступление в проект '([^']*)' от лица команды '([^']*)'")]
+    public async Task WhenПользовательОтправляетЗаявкуНаВступлениеВПроект(string username, string projectName , string teamName)
+    {
+
+    }
+
+    [Then(@" проекте '([^']*)' существует заявка на вступление от команды '([^']*)'")]
+    public async Task ThenВПроектеСуществуетЗаявкаНаВступлениеОтКоманды(int teamCount)
+    {
+
+    }
+}
