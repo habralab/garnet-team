103d536e1924aca2d9d463845d10ecb14d4e765a
gfg7 <nakonechnaya040702@gmail.com>
2023-10-07 15:19:40 +0500

    #17 set steps

--- features/team/server/Garnet.Teams.AcceptanceTests/Features/TeamJoinInvite/TeamJoinInviteSteps.cs
+++ features/team/server/Garnet.Teams.AcceptanceTests/Features/TeamJoinInvite/TeamJoinInviteSteps.cs
@@ -6,5 +6,23 @@ namespace Garnet.Teams.AcceptanceTests.Features.TeamJoinInvite
         public TeamJoinInviteSteps(StepsArgs args) : base(args)
         {
         }
+
+        [When(@"пользователь '(.*)' приглашает '(.*)' в команду '(.*)'")]
+        public Task WhenПользовательПриглашаетВКоманду(string teamOwner, string username, string teamName)
+        {
+            return Task.CompletedTask;
+        }
+
+        [Then(@"у пользователя '(.*)' количество приглашений в команды равно '(.*)'")]
+        public Task ThenУПользователяКоличествоПриглашенийВКомандыРавно(string username, int joinInviteCount)
+        {
+            return Task.CompletedTask;
+        }
+
+        [Given(@"существует приглашение пользователя '(.*)' на вступление в команду '(.*)' от владельца")]
+        public Task GivenСущесвуетПриглашениеПользователяНаВступлениеВКомандуОтВладельца(string username, string teamName)
+        {
+            return Task.CompletedTask;
+        }
     }
 }
