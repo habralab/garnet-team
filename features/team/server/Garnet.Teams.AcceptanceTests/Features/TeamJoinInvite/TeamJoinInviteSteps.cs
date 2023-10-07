1a7b9dec87a528cabe5a6762735caee283a70aff
gfg7 <nakonechnaya040702@gmail.com>
2023-10-07 15:06:26 +0500

    #17 init test proj

---
+++ features/team/server/Garnet.Teams.AcceptanceTests/Features/TeamJoinInvite/TeamJoinInviteSteps.cs
@@ -0,0 +1,10 @@
+namespace Garnet.Teams.AcceptanceTests.Features.TeamJoinInvite
+{
+    [Binding]
+    public class TeamJoinInviteSteps : BaseSteps
+    {
+        public TeamJoinInviteSteps(StepsArgs args) : base(args)
+        {
+        }
+    }
+}
