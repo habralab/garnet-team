b1ec3543f27a2142a23e3c47a4071386e0b4f9aa
gfg7 <nakonechnaya040702@gmail.com>
2023-10-07 20:30:09 +0500

    #42 set steps

--- features/team/server/Garnet.Teams.AcceptanceTests/Features/TeamJoinProjectRequest/TeamJoinProjectRequestSteps.cs
+++ features/team/server/Garnet.Teams.AcceptanceTests/Features/TeamJoinProjectRequest/TeamJoinProjectRequestSteps.cs
@@ -6,5 +6,11 @@ namespace Garnet.Teams.AcceptanceTests.Features.TeamJoinProjectRequest
         public TeamJoinProjectRequestSteps(StepsArgs args) : base(args)
         {
         }
+
+        [When(@"пользователь '(.*)' отправляет заявку на вступление в проект '(.*)' от лица команды '(.*)'")]
+        public Task WhenПользовательОтправляетЗаявкуНаВступлениеВПроектОтЛицаКоманды(string username, string projectName, string teamName)
+        {
+            return Task.CompletedTask;
+        }
     }
 }
