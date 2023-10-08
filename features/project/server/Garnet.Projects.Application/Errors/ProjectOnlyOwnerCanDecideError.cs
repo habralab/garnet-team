61587eb139951bb9af7f8b1611d5033800887f26
Demorey <demorey2@gmail.com>
2023-10-08 18:12:16 +0300

    #46 add errors

---
+++ features/project/server/Garnet.Projects.Application/Errors/ProjectOnlyOwnerCanDecideError.cs
@@ -0,0 +1,13 @@
+﻿using Garnet.Common.Application.Errors;
+
+namespace Garnet.Projects.Application.Errors;
+
+public class ProjectOnlyOwnerCanDecideError : ApplicationError
+{
+    public ProjectOnlyOwnerCanDecideError()
+        : base("Выносить вердикт по заявкам на вступление может только владелец этого проекта")
+    {
+    }
+
+    public override string Code => nameof(ProjectOnlyOwnerCanDecideError);
+}
