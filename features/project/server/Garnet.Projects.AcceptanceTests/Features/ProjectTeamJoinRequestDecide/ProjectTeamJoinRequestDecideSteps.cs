1cba5ce6e8c8cd82348dddbf00b8dac9272b85f0
Demorey <demorey2@gmail.com>
2023-10-08 18:14:41 +0300

    #46 finish test steps

--- features/project/server/Garnet.Projects.AcceptanceTests/Features/ProjectTeamJoinRequestDecide/ProjectTeamJoinRequestDecideSteps.cs
+++ features/project/server/Garnet.Projects.AcceptanceTests/Features/ProjectTeamJoinRequestDecide/ProjectTeamJoinRequestDecideSteps.cs
@@ -1,6 +1,10 @@
-﻿using Garnet.Common.AcceptanceTests.Fakes;
+﻿using FluentAssertions;
+using Garnet.Common.AcceptanceTests.Contexts;
+using Garnet.Common.AcceptanceTests.Fakes;
 using Garnet.Projects.Application;
+using Garnet.Projects.Infrastructure.Api.ProjectTeamJoinRequestDecide;
 using Garnet.Projects.Infrastructure.MongoDb;
+using HotChocolate.Execution;
 using MongoDB.Driver;
 using TechTalk.SpecFlow;
 
@@ -10,21 +14,24 @@ namespace Garnet.Projects.AcceptanceTests.Features.ProjectTeamJoinRequestDecide;
 public class ProjectTeamJoinRequestDecideSteps : BaseSteps
 {
     private readonly CurrentUserProviderFake _currentUserProviderFake;
+    private readonly QueryExceptionsContext _queryExceptionsContext;
 
-
-    private readonly FilterDefinitionBuilder<ProjectTeamDocument> _f =
-        Builders<ProjectTeamDocument>.Filter;
-
-    private readonly UpdateDefinitionBuilder<ProjectTeamDocument> _u =
-        Builders<ProjectTeamDocument>.Update;
-
-    private readonly IProjectTeamJoinRequestRepository _repository;
 
     public ProjectTeamJoinRequestDecideSteps(StepsArgs args, CurrentUserProviderFake currentUserProviderFake,
-        IProjectTeamJoinRequestRepository repository) : base(args)
+        QueryExceptionsContext queryExceptionsContext) : base(args)
     {
         _currentUserProviderFake = currentUserProviderFake;
-        _repository = repository;
+        _queryExceptionsContext = queryExceptionsContext;
+    }
+
+    private async Task<ProjectTeamJoinRequestDecideInput> SetJoinRequestDecision(string projectName, string teamName,
+        bool decision)
+    {
+        var project = await Db.Projects.Find(x => x.ProjectName == projectName).FirstAsync();
+        var userJoinRequest = await Db.ProjectTeamJoinRequests
+            .Find(x => x.ProjectId == project.Id && x.TeamName == teamName).FirstAsync();
+
+        return new ProjectTeamJoinRequestDecideInput(userJoinRequest.Id, decision);
     }
 
 
@@ -32,26 +39,59 @@ public class ProjectTeamJoinRequestDecideSteps : BaseSteps
     public async Task WhenПользовательПринимаетЗаявкуНаВступлениеВПроект(string username, string teamName,
         string projectName)
     {
+        var input = await SetJoinRequestDecision(projectName, teamName, true);
+        var claims = _currentUserProviderFake.LoginAs(username);
+
+        try
+        {
+            await Mutation.ProjectTeamJoinRequestDecide(CancellationToken.None, claims, input);
+        }
+        catch (QueryException ex)
+        {
+            _queryExceptionsContext.QueryExceptions.Add(ex);
+        }
     }
 
     [Then(@"команда '([^']*)' является участником проекта '([^']*)'")]
     public async Task ThenКомандаЯвляетсяУчастникомПроекта(string teamName, string projectName)
     {
+        var project = await Db.Projects.Find(x => x.ProjectName == projectName).FirstAsync();
+        var teamParticipant = await Db.ProjectTeamsParticipants
+            .Find(x => x.TeamName == teamName & x.ProjectId == project.Id).FirstAsync();
+        teamParticipant.Should().NotBeNull();
     }
 
     [Then(@"количество заявок на вступление в проект '([^']*)' равно '([^']*)'")]
-    public async Task ThenКомандчастникомПроекта(string projectName, int teamJoinRequestsCount)
+    public async Task ThenКоличествоЗаявокНаВступлениеВПроектРавно(string projectName, int teamJoinRequestsCount)
     {
+        var project = await Db.Projects.Find(x => x.ProjectName == projectName).FirstAsync();
+        var teamJoinRequests = await Db.ProjectTeamJoinRequests.Find(x => x.ProjectId == project.Id).ToListAsync();
+        teamJoinRequests.Count().Should().Be(teamJoinRequestsCount);
     }
 
     [When(@"пользователь '([^']*)' отклоняет заявку на участие от команды '([^']*)' в проект '([^']*)'")]
     public async Task WhenПользовательОтклоняетЗаявкуНаВступлениеВПроект(string username, string teamName,
         string projectName)
     {
+        var input = await SetJoinRequestDecision(projectName, teamName, false);
+        var claims = _currentUserProviderFake.LoginAs(username);
+
+        try
+        {
+            await Mutation.ProjectTeamJoinRequestDecide(CancellationToken.None, claims, input);
+        }
+        catch (QueryException ex)
+        {
+            _queryExceptionsContext.QueryExceptions.Add(ex);
+        }
     }
 
     [Then(@"команда '([^']*)' не является участником проекта '([^']*)'")]
     public async Task ThenКомандаНеЯвляетсяУчастникомПроекта(string teamName, string projectName)
     {
+        var project = await Db.Projects.Find(x => x.ProjectName == projectName).FirstAsync();
+        var teamParticipant = await Db.ProjectTeamsParticipants
+            .Find(x => x.TeamName == teamName & x.ProjectId == project.Id).FirstOrDefaultAsync();
+        teamParticipant.Should().BeNull();
     }
 }
