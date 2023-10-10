d0f2ec1993fe4c937452ce8d8671649afc6870c7
Demorey <demorey2@gmail.com>
2023-10-10 23:38:17 +0300

    #75 fix ProjectTeamUpdate

--- features/project/server/Garnet.Projects.Application/ProjectTeam/Commands/ProjectTeamUpdateCommand.cs
+++ features/project/server/Garnet.Projects.Application/ProjectTeam/Commands/ProjectTeamUpdateCommand.cs
@@ -1,17 +1,17 @@
 ﻿using Garnet.Projects.Application.ProjectTeam.Args;
 
-namespace Garnet.Projects.Application.ProjectTeam.Queries;
+namespace Garnet.Projects.Application.ProjectTeam.Commands;
 
-public class ProjectTeamUpdateQuery
+public class ProjectTeamUpdateCommand
 {
     private readonly IProjectTeamRepository _projectTeamRepository;
 
-    public ProjectTeamUpdateQuery(IProjectTeamRepository projectTeamRepository)
+    public ProjectTeamUpdateCommand(IProjectTeamRepository projectTeamRepository)
     {
         _projectTeamRepository = projectTeamRepository;
     }
 
-    public async Task<ProjectTeamEntity> Query(CancellationToken ct, ProjectTeamUpdateArgs args)
+    public async Task<ProjectTeamEntity> Execute(CancellationToken ct, ProjectTeamUpdateArgs args)
     {
         return await _projectTeamRepository.UpdateProjectTeam(ct, args.TeamId, args.TeamName, args.OwnerUserId);
     }
