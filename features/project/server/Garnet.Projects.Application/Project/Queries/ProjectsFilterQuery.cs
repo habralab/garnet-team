b171474a8b4c722bad4dd2a33a2a47cd399177b0
Demorey <demorey2@gmail.com>
2023-10-10 22:48:49 +0300

    #75 fix method name

--- features/project/server/Garnet.Projects.Application/Project/Queries/ProjectsFilterQuery.cs
+++ features/project/server/Garnet.Projects.Application/Project/Queries/ProjectsFilterQuery.cs
@@ -1,7 +1,5 @@
 ﻿using FluentResults;
-using Garnet.Common.Application.MessageBus;
 using Garnet.Projects.Application.Args;
-using Garnet.Projects.Application.ProjectUser;
 
 namespace Garnet.Projects.Application.Project.Queries;
 
@@ -15,7 +13,7 @@ public class ProjectsFilterQuery
         _projectRepository = projectRepository;
     }
 
-    public async Task<Result<ProjectEntity[]>> Execute(CancellationToken ct, ProjectFilterArgs args)
+    public async Task<Result<ProjectEntity[]>> Query(CancellationToken ct, ProjectFilterArgs args)
     {
         return await _projectRepository.FilterProjects(ct, args);
     }
