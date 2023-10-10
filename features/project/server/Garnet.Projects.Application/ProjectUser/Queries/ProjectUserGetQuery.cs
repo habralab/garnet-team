37814832df54f0dd194867f9f4cc777eb62ee6ac
Demorey <demorey2@gmail.com>
2023-10-10 22:18:30 +0300

    #75 refactored project user service

---
+++ features/project/server/Garnet.Projects.Application/ProjectUser/Queries/ProjectUserGetQuery.cs
@@ -0,0 +1,16 @@
+﻿namespace Garnet.Projects.Application.ProjectUser.Queries;
+
+public class ProjectUserGetQuery
+{
+    private readonly IProjectUserRepository _projectUserRepository;
+
+    public ProjectUserGetQuery(IProjectUserRepository projectUserRepository)
+    {
+        _projectUserRepository = projectUserRepository;
+    }
+
+    public async Task<ProjectUserEntity?> Query(CancellationToken ct, string userId)
+    {
+        return await _projectUserRepository.GetUser(ct, userId);
+    }
+}
