ea7a21b9f18bb9545c8fab37483f6a3fed841f1b
Demorey <demorey2@gmail.com>
2023-10-12 18:58:52 +0300

    #83 add ProjectUploadAvatar input and payload

---
+++ features/project/server/Garnet.Projects.Infrastructure/Api/ProjectUploadAvatar/ProjectUploadAvatarPayload.cs
@@ -0,0 +1,10 @@
+﻿namespace Garnet.Projects.Infrastructure.Api.ProjectUploadAvatar;
+
+public record ProjectUploadAvatarPayload(
+    string Id,
+    string OwnerUserId,
+    string ProjectName,
+    string? Description,
+    string? AvatarUrl,
+    string[] Tags
+    );
