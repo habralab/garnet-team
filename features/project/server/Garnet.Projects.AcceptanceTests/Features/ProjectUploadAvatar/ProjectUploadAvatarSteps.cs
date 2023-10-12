e4db17c989e49511be9790e3f432ae308704e9d5
Demorey <demorey2@gmail.com>
2023-10-12 15:27:08 +0300

    #83 add test scenario and steps

---
+++ features/project/server/Garnet.Projects.AcceptanceTests/Features/ProjectUploadAvatar/ProjectUploadAvatarSteps.cs
@@ -0,0 +1,52 @@
+﻿using Garnet.Common.AcceptanceTests.Contexts;
+using Garnet.Common.AcceptanceTests.Fakes;
+using Garnet.Projects.Infrastructure.Api.ProjectEdit;
+using TechTalk.SpecFlow;
+
+namespace Garnet.Projects.AcceptanceTests.Features.ProjectUploadAvatar;
+
+
+[Binding]
+public class ProjectUploadAvatarSteps : BaseSteps
+{
+    private readonly CurrentUserProviderFake _currentUserProviderFake;
+    private QueryExceptionsContext _errorStepContext = null!;
+
+    public ProjectUploadAvatarSteps(QueryExceptionsContext errorStepContext, CurrentUserProviderFake currentUserProviderFake,
+        StepsArgs args, ProjectEditDescriptionPayload? response) : base(args)
+    {
+        _errorStepContext = errorStepContext;
+        _currentUserProviderFake = currentUserProviderFake;
+    }
+
+    [Given(@"аватаркой проекта '(.*)' является ссылка '(.*)'")]
+    public async Task GivenАватаркойПроектаЯвляетсяСсылка(string projectName, string avatarUrl)
+    {
+
+    }
+
+    [When(@"'(.*)' редактирует аватарку проекта '(.*)' на '(.*)'")]
+    public async Task WhenПользовательРедактируетАватаркуПроекта(string username, string projectName,
+        string avatarFile)
+    {
+
+    }
+
+    [Then(@"аватарка проекта '(.*)' является '(.*)'")]
+    public async Task ThenАватаркоаПроектаЯвляется(string projectName, string avatarUrl)
+    {
+
+    }
+
+    [Then(@"в удаленном хранилище для проекта '(.*)' есть файл '(.*)'")]
+    public async Task ThenВУдаленномХранилищеДляПроектаЕстьФайл(string projectName, string avatarUrl)
+    {
+
+    }
+
+    [Then(@"в системе аватарка проекта '(.*)' содержит ссылку '(.*)'")]
+    public async Task ThenВСистемеАватаркаПроектаСодержитСсылку(string projectName, string avatarUrl)
+    {
+
+    }
+}
