692f3218cf67c3c03dd43eaf7a25cd24033cee6d
gfg7 <nakonechnaya040702@gmail.com>
2023-10-14 02:18:47 +0500

    #87 set steps

--- features/team/server/Garnet.Teams.AcceptanceTests/Features/TeamEditTags/TeamEditTagsSteps.cs
+++ features/team/server/Garnet.Teams.AcceptanceTests/Features/TeamEditTags/TeamEditTagsSteps.cs
@@ -1,3 +1,6 @@
+using FluentAssertions;
+using MongoDB.Driver;
+
 namespace Garnet.Teams.AcceptanceTests.Features.TeamEditTags
 {
     [Binding]
@@ -5,6 +8,20 @@ namespace Garnet.Teams.AcceptanceTests.Features.TeamEditTags
     {
         public TeamEditTagsSteps(StepsArgs args) : base(args)
         {
+        }
+
+        [When(@"'(.*)' редактирует теги команды '(.*)' на '(.*)'")]
+        public Task WhenРедактируетТегиКомандыНа(string username, string teamName, string tags)
+        {
+            return Task.CompletedTask;
+        }
+
+        [Then(@"теги команды '(.*)' состоят из '(.*)'")]
+        public async Task ThenТегиКомандыСостоятИз(string teamName, string tags)
+        {
+            var expectedTags = tags.Split(',', StringSplitOptions.TrimEntries);
+            var team = await Db.Teams.Find(x => x.Name == teamName).FirstAsync();
+            team.Tags.Should().BeEquivalentTo(tags);
         }
     }
 }
