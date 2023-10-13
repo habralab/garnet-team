6c2870906c8016561adb2256976a71a48f595e9e
gfg7 <nakonechnaya040702@gmail.com>
2023-10-14 02:05:05 +0500

    #87 init test proj

---
+++ features/team/server/Garnet.Teams.AcceptanceTests/Features/TeamEditTags/TeamEditTagsSteps.cs
@@ -0,0 +1,10 @@
+namespace Garnet.Teams.AcceptanceTests.Features.TeamEditTags
+{
+    [Binding]
+    public class TeamEditTagsSteps : BaseSteps
+    {
+        public TeamEditTagsSteps(StepsArgs args) : base(args)
+        {
+        }
+    }
+}
