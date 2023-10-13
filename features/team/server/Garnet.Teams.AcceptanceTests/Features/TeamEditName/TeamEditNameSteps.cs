be8453061ba548444d2e016193e412379071aafb
gfg7 <nakonechnaya040702@gmail.com>
2023-10-13 20:55:38 +0500

    #86 init test proj

---
+++ features/team/server/Garnet.Teams.AcceptanceTests/Features/TeamEditName/TeamEditNameSteps.cs
@@ -0,0 +1,9 @@
+namespace Garnet.Teams.AcceptanceTests.Features.TeamEditName
+{
+    public class TeamEditNameSteps : BaseSteps
+    {
+        public TeamEditNameSteps(StepsArgs args) : base(args)
+        {
+        }
+    }
+}
