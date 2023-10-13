bd7e6bc72531ef80f817b3d77c888e1099e90923
gfg7 <nakonechnaya040702@gmail.com>
2023-10-13 23:43:29 +0500

    #86 set edit team name repository

--- features/team/server/Garnet.Teams.Infrastructure/MongoDb/Team/TeamRepository.cs
+++ features/team/server/Garnet.Teams.Infrastructure/MongoDb/Team/TeamRepository.cs
@@ -133,9 +133,21 @@ namespace Garnet.Teams.Infrastructure.MongoDb.Team
             return team is null ? null : TeamDocument.ToDomain(team);
         }
 
-        public Task<TeamEntity?> EditTeamName(CancellationToken ct, string teamId, string name)
+        public async Task<TeamEntity?> EditTeamName(CancellationToken ct, string teamId, string name)
         {
-            throw new NotImplementedException();
+            var db = _dbFactory.Create();
+
+            var team = await db.Teams.FindOneAndUpdateAsync(
+                _f.Eq(x => x.Id, teamId),
+                _u.Set(x => x.Name, name),
+                options: new FindOneAndUpdateOptions<TeamDocument>
+                {
+                    ReturnDocument = ReturnDocument.After
+                },
+                cancellationToken: ct
+            );
+
+            return team is null ? null : TeamDocument.ToDomain(team);
         }
     }
 }
