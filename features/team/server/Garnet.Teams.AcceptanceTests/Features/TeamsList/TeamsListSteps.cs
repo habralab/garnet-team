f08fb95184f5043413d9b3d4b5dd76bf02543169
gfg7 <nakonechnaya040702@gmail.com>
2023-10-15 00:21:57 +0500

    #79 set steps

--- features/team/server/Garnet.Teams.AcceptanceTests/Features/TeamsList/TeamsListSteps.cs
+++ features/team/server/Garnet.Teams.AcceptanceTests/Features/TeamsList/TeamsListSteps.cs
@@ -6,5 +6,17 @@ namespace Garnet.Teams.AcceptanceTests.Features.TeamsList
         public TeamsListSteps(StepsArgs args) : base(args)
         {
         }
+
+        [When(@"производится запрос списка команд пользователя '(.*)'")]
+        public Task WhenПроизводитсяЗапросСпискаКомандПользователя(string вася)
+        {
+            return Task.CompletedTask;
+        }
+
+        [Then(@"количество результатов списка команд пользователя равно '(.*)'")]
+        public Task ThenКоличествоРезультатовСпискаКомандПользователяРавно(string p0)
+        {
+            return Task.CompletedTask;
+        }
     }
 }
