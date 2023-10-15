3a7c816b6a14fb58348c0e125e7e61fddd27947f
Demorey <demorey2@gmail.com>
2023-10-15 20:07:17 +0300

    #102 add error

---
+++ features/project/server/Garnet.Projects.Application/Project/Errors/ProjectOnlyNameCanEditError.cs
@@ -0,0 +1,12 @@
+﻿using Garnet.Common.Application.Errors;
+
+namespace Garnet.Projects.Application.Project.Errors;
+
+internal class ProjectOnlyNameCanEditError : ApplicationError
+{
+    public ProjectOnlyNameCanEditError() : base("Название проекта может отредактировать только его владелец")
+    {
+    }
+
+    public override string Code => nameof(ProjectOnlyNameCanEditError);
+}
