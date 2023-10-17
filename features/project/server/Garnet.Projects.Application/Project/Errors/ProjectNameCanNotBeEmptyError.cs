c5f54cf43839bd91bf97f74bed1e4c550895b15e
Demorey <demorey2@gmail.com>
2023-10-17 20:02:29 +0300

    #112 add error

---
+++ features/project/server/Garnet.Projects.Application/Project/Errors/ProjectNameCanNotBeEmptyError.cs
@@ -0,0 +1,12 @@
+﻿using Garnet.Common.Application.Errors;
+
+namespace Garnet.Projects.Application.Project.Errors;
+
+public class ProjectNameCanNotBeEmptyError : ApplicationError
+{
+    public ProjectNameCanNotBeEmptyError() : base("Название проекта не может быть пустым")
+    {
+    }
+
+    public override string Code => nameof(ProjectNameCanNotBeEmptyError);
+}
