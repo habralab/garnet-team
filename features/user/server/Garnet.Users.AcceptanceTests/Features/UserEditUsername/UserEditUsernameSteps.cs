0d2aa99d1d76734fd79cbb38fa1dd5c2d6fed053
gfg7 <nakonechnaya040702@gmail.com>
2023-10-18 00:20:02 +0500

    #65 set steps

--- features/user/server/Garnet.Users.AcceptanceTests/Features/UserEditUsername/UserEditUsernameSteps.cs
+++ features/user/server/Garnet.Users.AcceptanceTests/Features/UserEditUsername/UserEditUsernameSteps.cs
@@ -1,3 +1,6 @@
+using FluentAssertions;
+using MongoDB.Driver;
+
 namespace Garnet.Users.AcceptanceTests.Features.UserEditUsername
 {
     [Binding]
@@ -6,5 +9,19 @@ namespace Garnet.Users.AcceptanceTests.Features.UserEditUsername
         public UserEditUsernameSteps(StepsArgs args) : base(args)
         {
         }
+
+        [When(@"пользователь '(.*)' меняет в своем профиле ник на '(.*)'")]
+        public Task WhenПользовательМеняетВСвоемПрофилеНикНа(string username, string newUsername)
+        {
+            return Task.CompletedTask;
+        }
+
+        [Then(@"в системе есть пользователь с ником '(.*)'")]
+        public async Task ThenВСистемеЕстьПользовательСНиком(string username)
+        {
+            var user = await Db.Users.Find(x => x.UserName == username).FirstOrDefaultAsync();
+            user.Should().NotBeNull();
+        }
+
     }
 }
