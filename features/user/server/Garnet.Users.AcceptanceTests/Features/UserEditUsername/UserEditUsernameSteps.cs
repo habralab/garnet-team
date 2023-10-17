9525ac6634c05469ff6c06f16c063ff1b864b76b
gfg7 <nakonechnaya040702@gmail.com>
2023-10-18 00:15:35 +0500

    #65 init test proj

---
+++ features/user/server/Garnet.Users.AcceptanceTests/Features/UserEditUsername/UserEditUsernameSteps.cs
@@ -0,0 +1,10 @@
+namespace Garnet.Users.AcceptanceTests.Features.UserEditUsername
+{
+    [Binding]
+    public class UserEditUsernameSteps : BaseSteps
+    {
+        public UserEditUsernameSteps(StepsArgs args) : base(args)
+        {
+        }
+    }
+}
