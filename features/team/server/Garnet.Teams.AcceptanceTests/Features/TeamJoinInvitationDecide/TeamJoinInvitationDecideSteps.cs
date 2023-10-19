2f9da595e33cd6b98e1abc3bd905093a5d60f795
gfg7 <nakonechnaya040702@gmail.com>
2023-10-20 02:56:18 +0500

    #122 set steps

--- features/team/server/Garnet.Teams.AcceptanceTests/Features/TeamJoinInvitationDecide/TeamJoinInvitationDecideSteps.cs
+++ features/team/server/Garnet.Teams.AcceptanceTests/Features/TeamJoinInvitationDecide/TeamJoinInvitationDecideSteps.cs
@@ -6,5 +6,17 @@ namespace Garnet.Teams.AcceptanceTests.Features.TeamJoinInvitationDecide
         public TeamJoinInvitationDecideSteps(StepsArgs args) : base(args)
         {
         }
+
+        [When(@"'(.*)' принимает приглашение в команду '(.*)'")]
+        public Task WhenПринимаетПриглашениеВКоманду(string маша0, string fooBar1)
+        {
+            return Task.CompletedTask;
+        }
+
+        [When(@"'(.*)' отклоняет приглашение в команду '(.*)'")]
+        public Task WhenОтклоняетПриглашениеВКоманду(string маша0, string fooBar1)
+        {
+            return Task.CompletedTask;
+        }
     }
 }
