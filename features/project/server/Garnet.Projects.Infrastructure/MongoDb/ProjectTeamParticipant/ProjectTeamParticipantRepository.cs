7d311845b63de8c221a60d57da026fd6a32d92e4
Demorey <demorey2@gmail.com>
2023-10-20 17:37:37 +0300

    #111 edit team participant create method

--- features/project/server/Garnet.Projects.Infrastructure/MongoDb/ProjectTeamParticipant/ProjectTeamParticipantRepository.cs
+++ features/project/server/Garnet.Projects.Infrastructure/MongoDb/ProjectTeamParticipant/ProjectTeamParticipantRepository.cs
@@ -1,5 +1,7 @@
 ﻿using Garnet.Common.Infrastructure.Support;
+using Garnet.Projects.Application.Project;
 using Garnet.Projects.Application.ProjectTeamParticipant;
+using Garnet.Projects.Infrastructure.MongoDb.Project;
 using MongoDB.Driver;
 
 namespace Garnet.Projects.Infrastructure.MongoDb.ProjectTeamParticipant;
@@ -24,10 +26,30 @@ public class ProjectTeamParticipantRepository : IProjectTeamParticipantRepositor
         string teamName, string projectId)
     {
         var db = _dbFactory.Create();
-        var team = ProjectTeamParticipantDocument.Create(Uuid.NewMongo(), teamId, teamName, projectId);
-        await db.ProjectTeamsParticipants.InsertOneAsync(team, cancellationToken: ct);
+        var team = await db.ProjectTeams.Find(x => x.Id == teamId).FirstAsync(ct);
+        var userParticipantsIds = team.UserParticipantsId;
 
-        return ProjectTeamParticipantDocument.ToDomain(team);
+        var userParticipants = new List<UserParticipant>();
+        foreach (var userId in userParticipantsIds)
+        {
+            var user = await db.ProjectUsers.Find(x => x.Id == userId).FirstAsync(ct);
+            userParticipants.Add(new UserParticipant(user.Id, user.UserName, user.UserAvatarUrl));
+        }
+
+        var projectsIdOfTeamParticipant =
+            await db.ProjectTeamsParticipants.Find(x => x.TeamId == teamId).ToListAsync(ct);
+        var projectsList = new List<ProjectEntity>();
+        foreach (var projectIdOfTeamParticipant in projectsIdOfTeamParticipant)
+        {
+            var project = await db.Projects.Find(x => x.Id == projectIdOfTeamParticipant.ProjectId).FirstAsync(ct);
+            projectsList.Add(ProjectDocument.ToDomain(project));
+        }
+
+        var teamParticipant = ProjectTeamParticipantDocument.Create(Uuid.NewMongo(), teamId, teamName, projectId,
+            userParticipants.ToArray(), projectsList.ToArray());
+        await db.ProjectTeamsParticipants.InsertOneAsync(teamParticipant, cancellationToken: ct);
+
+        return ProjectTeamParticipantDocument.ToDomain(teamParticipant);
     }
 
     public async Task<ProjectTeamParticipantEntity[]> GetProjectTeamParticipantsByProjectId(CancellationToken ct,
