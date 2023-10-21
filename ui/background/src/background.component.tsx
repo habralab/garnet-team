d0f63acc6bca457f535721470147a4d4bbd6fc48
Dmitry <dimafomin02@gmail.com>
2023-10-21 16:39:21 +0300

    feat: added padding for Wrapper

--- ui/background/src/background.component.tsx
+++ ui/background/src/background.component.tsx
@@ -8,7 +8,10 @@ export const Background = styled(Box)<BackgroundProps>(({ theme, color }) => ({
   background: color ? theme.backgrounds[color] : 'none',
 }))
 
+const paddings = 30
+
 export const Wrapper = styled(Box)(() => ({
   width: '100%',
-  maxWidth: 1296,
+  maxWidth: 1296 + paddings,
+  padding: `0 ${paddings / 2}px`,
 }))
