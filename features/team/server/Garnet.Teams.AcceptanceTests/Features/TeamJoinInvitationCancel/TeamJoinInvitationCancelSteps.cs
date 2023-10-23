5e61ec4c8ddec7f1ad4f5173f7c910abd6778754
gfg7 <nakonechnaya040702@gmail.com>
2023-10-24 01:25:22 +0500

    #59 set steps

--- features/team/server/Garnet.Teams.AcceptanceTests/Features/TeamJoinInvitationCancel/TeamJoinInvitationCancelSteps.cs
+++ features/team/server/Garnet.Teams.AcceptanceTests/Features/TeamJoinInvitationCancel/TeamJoinInvitationCancelSteps.cs
@@ -1,3 +1,6 @@
+using FluentAssertions;
+using MongoDB.Driver;
+
 namespace Garnet.Teams.AcceptanceTests.Features.TeamJoinInvitationCancel
 {
     [Binding]
@@ -5,6 +8,21 @@ namespace Garnet.Teams.AcceptanceTests.Features.TeamJoinInvitationCancel
     {
         public TeamJoinInvitationCancelSteps(StepsArgs args) : base(args)
         {
+        }
+
+        [When(@"'(.*)' отменяет приглашение пользователя '(.*)' на вступление в команду '(.*)'")]
+        public Task WhenОтменяетПриглашениеПользователяНаВступлениеВКоманду(string ownerUsername, string username, string teamName)
+        {
+            return Task.CompletedTask;
+        }
+
+        [Then(@"в команде '(.*)' количество приглашений на вступление равно '(.*)'")]
+        public async Task ThenВКомандеКоличествоПриглашенийНаВступлениеРавно(string teamName, int invitationCount)
+        {
+            var team = await Db.Teams.Find(x => x.Name == teamName).FirstAsync();
+            var invitations = await Db.TeamJoinInvitations.Find(x => x.TeamId == team.Id).ToListAsync();
+
+            invitations.Count.Should().Be(invitationCount);
         }
     }
 }
