fb144c8763f2112f1de5e4e63f5ed6bb3ba6fa41
gfg7 <nakonechnaya040702@gmail.com>
2023-10-24 01:19:23 +0500

    #59 init test proj

---
+++ features/team/server/Garnet.Teams.AcceptanceTests/Features/TeamJoinInvitationCancel/TeamJoinInvitationCancelSteps.cs
@@ -0,0 +1,10 @@
+namespace Garnet.Teams.AcceptanceTests.Features.TeamJoinInvitationCancel
+{
+    [Binding]
+    public class TeamJoinInvitationCancelSteps : BaseSteps
+    {
+        public TeamJoinInvitationCancelSteps(StepsArgs args) : base(args)
+        {
+        }
+    }
+}
