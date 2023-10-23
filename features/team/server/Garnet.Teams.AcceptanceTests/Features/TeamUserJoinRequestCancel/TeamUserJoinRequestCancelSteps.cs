0150c840a41bbfb61250784768e0cc6069534e88
gfg7 <nakonechnaya040702@gmail.com>
2023-10-24 00:20:11 +0500

    #57 add test step

--- features/team/server/Garnet.Teams.AcceptanceTests/Features/TeamUserJoinRequestCancel/TeamUserJoinRequestCancelSteps.cs
+++ features/team/server/Garnet.Teams.AcceptanceTests/Features/TeamUserJoinRequestCancel/TeamUserJoinRequestCancelSteps.cs
@@ -1,10 +1,27 @@
+using Garnet.Common.AcceptanceTests.Contexts;
+using Garnet.Common.AcceptanceTests.Fakes;
+
 namespace Garnet.Teams.AcceptanceTests.Features.TeamUserJoinRequestCancel
 {
     [Binding]
     public class TeamUserJoinRequestCancelSteps : BaseSteps
     {
-        public TeamUserJoinRequestCancelSteps(StepsArgs args) : base(args)
+        private readonly QueryExceptionsContext _queryExceptionsContext;
+        private readonly CurrentUserProviderFake _currentUserProviderFake;
+
+        public TeamUserJoinRequestCancelSteps(
+            CurrentUserProviderFake currentUserProviderFake,
+            QueryExceptionsContext queryExceptionsContext,
+            StepsArgs args) : base(args)
         {
+            _currentUserProviderFake = currentUserProviderFake;
+            _queryExceptionsContext = queryExceptionsContext;
+        }
+
+        [When(@"'(.*)' отменяет заявку на вступление в '(.*)'")]
+        public Task WhenОтменяетЗаявкуНаВступлениеВ(string вася, string fooBar)
+        {
+            return Task.CompletedTask;
         }
     }
 }
