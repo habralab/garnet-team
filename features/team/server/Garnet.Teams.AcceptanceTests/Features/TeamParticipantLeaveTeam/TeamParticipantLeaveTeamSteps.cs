d589b089f65385add615c2254dac59dd6310aee8
gfg7 <nakonechnaya040702@gmail.com>
2023-10-24 01:54:09 +0500

    #88 init test proj

---
+++ features/team/server/Garnet.Teams.AcceptanceTests/Features/TeamParticipantLeaveTeam/TeamParticipantLeaveTeamSteps.cs
@@ -0,0 +1,10 @@
+namespace Garnet.Teams.AcceptanceTests.Features.TeamParticipantLeaveTeam
+{
+    [Binding]
+    public class TeamParticipantLeaveTeamSteps : BaseSteps
+    {
+        public TeamParticipantLeaveTeamSteps(StepsArgs args) : base(args)
+        {
+        }
+    }
+}
