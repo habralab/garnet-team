ef3f49513816d301e0685455373f876c0dfbc36c
gfg7 <nakonechnaya040702@gmail.com>
2023-10-24 02:54:41 +0500

    #89 set steps & add negative scenario

--- features/team/server/Garnet.Teams.AcceptanceTests/Features/TeamLeaveProject/TeamLeaveProjectSteps.cs
+++ features/team/server/Garnet.Teams.AcceptanceTests/Features/TeamLeaveProject/TeamLeaveProjectSteps.cs
@@ -6,5 +6,25 @@ namespace Garnet.Teams.AcceptanceTests.Features.TeamLeaveProject
         public TeamLeaveProjectSteps(StepsArgs args) : base(args)
         {
         }
+
+        [Given(@"команда '(.*)' является участником проекта '(.*)'")]
+        public Task GivenКомандаЯвляетсяУчастникомПроекта(string teamName, string project)
+        {
+            return Task.CompletedTask;
+        }
+
+        [When(@"'(.*)' удаляет команду '(.*)' из состава проекта '(.*)'")]
+        public Task WhenУдаляетКомандуИзСоставаПроекта(string username, string teamName, string project)
+        {
+            return Task.CompletedTask;
+
+        }
+
+        [Then(@"команда '(.*)' не является участником проекта '(.*)'")]
+        public Task ThenКомандаНеЯвляетсяУчастникомПроекта(string teamName, string project)
+        {
+            return Task.CompletedTask;
+
+        }
     }
 }
