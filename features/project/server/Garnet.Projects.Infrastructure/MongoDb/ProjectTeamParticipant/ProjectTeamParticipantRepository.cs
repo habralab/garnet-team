f0ab0635e7c6f6988566d1d31278e58bda3e467a
Demorey <demorey2@gmail.com>
2023-10-24 19:51:12 +0300

    #111 add project user participant update optimisation

--- features/project/server/Garnet.Projects.Infrastructure/MongoDb/ProjectTeamParticipant/ProjectTeamParticipantRepository.cs
+++ features/project/server/Garnet.Projects.Infrastructure/MongoDb/ProjectTeamParticipant/ProjectTeamParticipantRepository.cs
@@ -87,14 +87,11 @@ public class ProjectTeamParticipantRepository : IProjectTeamParticipantRepositor
         var db = _dbFactory.Create();
         var user = await db.ProjectUsers.Find(x => x.Id == userId).FirstAsync(ct);
         var userDoc = new ProjectUserEntity(user.Id, user.UserName, user.UserAvatarUrl);
-        var teams = await db.ProjectTeams.Find(x => x.Id == teamId).ToListAsync(ct);
-        foreach (var team in teams)
-        {
-            await db.ProjectTeamsParticipants.FindOneAndUpdateAsync(
-                _teamParticipantFilter.Eq(x => x.Id, teamId),
-                _u.AddToSet(x => x.UserParticipants, userDoc),
-                cancellationToken: ct
-            );
-        }
+
+        await db.ProjectTeamsParticipants.UpdateManyAsync(
+            _teamParticipantFilter.Eq(x => x.TeamId, teamId),
+            _u.AddToSet(x => x.UserParticipants, userDoc),
+            cancellationToken: ct
+        );
     }
 }
