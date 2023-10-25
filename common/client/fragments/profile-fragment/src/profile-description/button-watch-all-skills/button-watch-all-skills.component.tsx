c9fd4a6e07934055324d43bc84b02b352c6fda8c
Dmitry <dimafomin02@gmail.com>
2023-10-25 05:09:29 +0300

    refactor: moved modal-watch-all-skills to @shared/modals-fragment

--- common/client/fragments/profile-fragment/src/profile-description/button-watch-all-skills/button-watch-all-skills.component.tsx
+++ common/client/fragments/profile-fragment/src/profile-description/button-watch-all-skills/button-watch-all-skills.component.tsx
@@ -2,20 +2,15 @@ import React                         from 'react'
 import { FC }                        from 'react'
 import { FormattedMessage }          from 'react-intl'
 import { useState }                  from 'react'
-import { useIntl }                   from 'react-intl'
 
+import { ModalWatchAllSkills }       from '@shared/modals-fragment'
 import { Button }                    from '@ui/button'
-import { Layout }                    from '@ui/layout'
-import { Row }                       from '@ui/layout'
-import { Modal }                     from '@ui/modal'
-import { Tag }                       from '@ui/tag'
 import { Text }                      from '@ui/text'
 
 import { ButtonWatchAllSkillsProps } from './button-watch-all-skills.interfaces'
 
 export const ButtonWatchAllSkills: FC<ButtonWatchAllSkillsProps> = ({ skills }) => {
   const [modalOpen, setModalOpen] = useState(false)
-  const { formatMessage } = useIntl()
 
   const openModal = () => setModalOpen(true)
   const closeModal = () => setModalOpen(false)
@@ -27,21 +22,7 @@ export const ButtonWatchAllSkills: FC<ButtonWatchAllSkillsProps> = ({ skills }) 
           <FormattedMessage id='profile.watch_all' />
         </Text>
       </Button>
-      <Modal
-        open={modalOpen}
-        title={formatMessage({ id: 'profile.skills' })}
-        okText={formatMessage({ id: 'profile.close' })}
-        showCancel={false}
-        onClose={closeModal}
-        onOk={closeModal}
-      >
-        <Row flexWrap='wrap' style={{ gap: 10 }}>
-          {skills?.map((tag) => (
-            <Tag key={tag}>{tag}</Tag>
-          ))}
-        </Row>
-        <Layout flexBasis={50} flexShrink={0} />
-      </Modal>
+      <ModalWatchAllSkills modalOpen={modalOpen} onClose={closeModal} skills={skills} />
     </>
   )
 }
