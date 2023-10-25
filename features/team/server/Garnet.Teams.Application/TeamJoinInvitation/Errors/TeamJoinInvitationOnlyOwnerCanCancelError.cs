a2bba948bb9513eedc3c0674de4ab2a80f1569cf
gfg7 <nakonechnaya040702@gmail.com>
2023-10-25 23:50:58 +0500

    #59 fix error msg

--- features/team/server/Garnet.Teams.Application/TeamJoinInvitation/Errors/TeamJoinInvitationOnlyOwnerCanCancelError.cs
+++ features/team/server/Garnet.Teams.Application/TeamJoinInvitation/Errors/TeamJoinInvitationOnlyOwnerCanCancelError.cs
@@ -4,7 +4,7 @@ namespace Garnet.Teams.Application.TeamJoinInvitation.Errors
 {
     public class TeamJoinInvitationOnlyOwnerCanCancelError : ApplicationError
     {
-        public TeamJoinInvitationOnlyOwnerCanCancelError() : base("Отклонять приглашения на вступление в команду может только ее владелец")
+        public TeamJoinInvitationOnlyOwnerCanCancelError() : base("Отменять приглашения на вступление в команду может только ее владелец")
         {
         }
 
