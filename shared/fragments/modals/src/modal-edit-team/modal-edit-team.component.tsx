b734d69f621f5d6682286542f15e93bacf4d59d7
Dmitry <dimafomin02@gmail.com>
2023-10-25 16:46:36 +0300

    feat: added modal-edit-team

---
+++ shared/fragments/modals/src/modal-edit-team/modal-edit-team.component.tsx
@@ -0,0 +1,44 @@
+import React                  from 'react'
+import { FC }                 from 'react'
+import { useState }           from 'react'
+import { useIntl }            from 'react-intl'
+
+import { FormTeam }           from '@ui/form'
+import { FormTeamValues }     from '@ui/form'
+import { Layout }             from '@ui/layout'
+import { Modal }              from '@ui/modal'
+
+import { ModalEditTeamProps } from './modal-edit-team.interfaces'
+import { getFormValues }      from './helpers'
+
+export const ModalEditTeam: FC<ModalEditTeamProps> = ({ modalOpen = false, onClose, team }) => {
+  const [formValues, setFormValues] = useState<FormTeamValues>(getFormValues(team))
+  const { formatMessage } = useIntl()
+
+  const closeModal = () => {
+    setFormValues(getFormValues(team))
+    onClose?.()
+  }
+
+  const handleChange = (field: keyof FormTeamValues) => (value: string | string[]) => {
+    setFormValues({ ...formValues, [field]: value })
+  }
+
+  const handleSubmit = () => {
+    /** @todo submit form */
+    closeModal?.()
+  }
+
+  return (
+    <Modal
+      open={modalOpen}
+      title={formatMessage({ id: 'shared_ui.modal.edit_team' })}
+      onClose={closeModal}
+      onCancel={closeModal}
+      onOk={handleSubmit}
+    >
+      <FormTeam formValues={formValues} handleChange={handleChange} />
+      <Layout flexBasis={50} flexShrink={0} />
+    </Modal>
+  )
+}
