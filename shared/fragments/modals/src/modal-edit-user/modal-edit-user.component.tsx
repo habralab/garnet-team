23d0e596e5b75e295d3640864356e801bc879c8e
Dmitry <dimafomin02@gmail.com>
2023-10-25 16:18:02 +0300

    refactor: moved modal-edit-user to modals-fragment

---
+++ shared/fragments/modals/src/modal-edit-user/modal-edit-user.component.tsx
@@ -0,0 +1,44 @@
+import React                  from 'react'
+import { FC }                 from 'react'
+import { useState }           from 'react'
+import { useIntl }            from 'react-intl'
+
+import { FormUser }           from '@ui/form'
+import { FormUserValues }     from '@ui/form'
+import { Layout }             from '@ui/layout'
+import { Modal }              from '@ui/modal'
+
+import { ModalEditUserProps } from './modal-edit-user.interfaces'
+import { getFormValues }      from './helpers'
+
+export const ModalEditUser: FC<ModalEditUserProps> = ({ modalOpen = false, onClose, user }) => {
+  const [formValues, setFormValues] = useState<FormUserValues>(getFormValues(user))
+  const { formatMessage } = useIntl()
+
+  const closeModal = () => {
+    setFormValues(getFormValues(user))
+    onClose?.()
+  }
+
+  const handleChange = (field: keyof FormUserValues) => (value: string | string[]) => {
+    setFormValues({ ...formValues, [field]: value })
+  }
+
+  const handleSubmit = () => {
+    /** @todo submit form */
+    closeModal?.()
+  }
+
+  return (
+    <Modal
+      open={modalOpen}
+      title={formatMessage({ id: 'shared_ui.modal.edit_profile' })}
+      onClose={closeModal}
+      onCancel={closeModal}
+      onOk={handleSubmit}
+    >
+      <FormUser formValues={formValues} handleChange={handleChange} />
+      <Layout flexBasis={50} flexShrink={0} />
+    </Modal>
+  )
+}
