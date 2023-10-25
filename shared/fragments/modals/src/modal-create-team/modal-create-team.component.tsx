4abf6ad9d8ab087d97b4eb83e7e3a7246121bafb
Dmitry <dimafomin02@gmail.com>
2023-10-25 16:25:45 +0300

    refactor: moved modal-create-team to modals-fragment

---
+++ shared/fragments/modals/src/modal-create-team/modal-create-team.component.tsx
@@ -0,0 +1,45 @@
+import React                    from 'react'
+import { FC }                   from 'react'
+import { useState }             from 'react'
+import { useIntl }              from 'react-intl'
+
+import { FormTeam }             from '@ui/form'
+import { FormTeamValues }       from '@ui/form'
+import { Layout }               from '@ui/layout'
+import { Modal }                from '@ui/modal'
+
+import { ModalCreateTeamProps } from './modal-create-team.interfaces'
+
+const defaultFormValues: FormTeamValues = { name: '', description: '', tags: [], avatar: '' }
+
+export const ModalCreateTeam: FC<ModalCreateTeamProps> = ({ modalOpen = false, onClose }) => {
+  const [formValues, setFormValues] = useState<FormTeamValues>(defaultFormValues)
+  const { formatMessage } = useIntl()
+
+  const closeModal = () => {
+    setFormValues(defaultFormValues)
+    onClose?.()
+  }
+
+  const handleChange = (field: keyof FormTeamValues) => (value: string | string[]) => {
+    setFormValues({ ...formValues, [field]: value })
+  }
+
+  const handleSubmit = () => {
+    /** @todo submit form */
+    closeModal()
+  }
+
+  return (
+    <Modal
+      open={modalOpen}
+      title={formatMessage({ id: 'shared_ui.modal.create_team' })}
+      onClose={closeModal}
+      onCancel={closeModal}
+      onOk={handleSubmit}
+    >
+      <FormTeam formValues={formValues} handleChange={handleChange} />
+      <Layout flexBasis={50} flexShrink={0} />
+    </Modal>
+  )
+}
