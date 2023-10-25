4ddcfed2ae9ad28bdabbcfd505623f06c4a896c9
Demorey <demorey2@gmail.com>
2023-10-25 22:59:25 +0300

    #140 add repository interface

---
+++ features/project/server/Garnet.Projects.Application/ProjectTask/IProjectTaskRepository.cs
@@ -0,0 +1,8 @@
+﻿using Garnet.Projects.Application.ProjectTask.Args;
+
+namespace Garnet.Projects.Application.ProjectTask;
+
+public interface IProjectTaskRepository
+{
+    Task<ProjectTaskEntity> CreateProjectTask(CancellationToken ct,string userCreatorId, ProjectTaskCreateArgs args);
+}
