f189379a0b984ddf76aa1a5af012e485129e5002
Demorey <demorey2@gmail.com>
2023-10-25 22:58:53 +0300

    #140 add events

---
+++ features/project/server/Garnet.Projects.Events/ProjectTask/ProjectTaskUpdatedEvent.cs
@@ -0,0 +1,13 @@
+﻿namespace Garnet.Projects.Events.ProjectTask;
+
+public record ProjectTaskUpdatedEvent(
+    string Id,
+    string ProjectId,
+    string UserCreatorId,
+    string Name,
+    string? Description,
+    string Status,
+    string? TeamExecutorId,
+    string? UserExecutorId,
+    string[] Tags
+);
