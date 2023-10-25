1ce1ec150cfd08b3b1b9c8f6939ac2028865edeb
Dmitry <dimafomin02@gmail.com>
2023-10-25 17:26:35 +0300

    refactor: changed mock data

--- shared/data/src/mock.ts
+++ shared/data/src/mock.ts
@@ -1,4 +1,5 @@
 import { MockUser } from './data.interfaces'
+import { Team }     from './data.interfaces'
 import { Project }  from './data.interfaces'
 
 export const mockAuthUserId = '6516e88d723450ac19495148'
@@ -110,9 +111,34 @@ export const getMockUser = (id: string) => {
 export const mockProjects: Project[] = Array.from({ length: 20 }, (_, index) => ({
   id: String(index),
   name: `Dream Project ${index}`,
+  description:
+    'Идейные соображения высшего порядка, а также постоянное информационно-пропагандистское обеспечение нашей деятельности играет важную роль в формировании системы обучения кадров.',
+  ownerUserId: index % 2 === 0 ? mockAuthUserId : mockNotAuthUserId,
   countTeams: index + 1,
   countUsers: index + 1,
   avatarUrl:
-    'https://is4-ssl.mzstatic.com/image/thumb/Music112/v4/2e/3a/7f/2e3a7f87-761b-0113-9f01-dd4fc4470dcf/5059801037680_cover.jpg/1200x1200bf-60.jpg',
+    index % 2 === 0
+      ? 'https://is4-ssl.mzstatic.com/image/thumb/Music112/v4/2e/3a/7f/2e3a7f87-761b-0113-9f01-dd4fc4470dcf/5059801037680_cover.jpg/1200x1200bf-60.jpg'
+      : 'https://is4-ssl.mzstatic.com/image/thumb/Music122/v4/97/65/ae/9765ae34-5f56-cd3e-cfb2-6027efeb1d10/5063010116104_cover.jpg/800x800bb.jpg',
   tags: mockTags,
 }))
+
+export const mockTeams: Team[] = Array.from({ length: 20 }, (_, index) => ({
+  id: String(index),
+  name: `Dream Team ${index}`,
+  description:
+    'Идейные соображения высшего порядка, а также постоянное информационно-пропагандистское обеспечение нашей деятельности играет важную роль в формировании системы обучения кадров.',
+  ownerUserId: index % 2 === 0 ? mockAuthUserId : mockNotAuthUserId,
+  countProjects: index + 1,
+  countUsers: index + 1,
+  avatarUrl:
+    index % 2 === 0
+      ? 'https://i.pinimg.com/originals/59/18/00/5918007654bb37ae44692437ba3b2c6d.jpg'
+      : 'https://cdns-images.dzcdn.net/images/cover/2012895b602fffa8ce2e061b20bed087/1000x1000.jpg',
+  tags: mockTags,
+}))
+
+export const getMockProject = (id: string) => ({
+  project: mockProjects.find((project) => project.id === id),
+  projectTeams: mockTeams,
+})
