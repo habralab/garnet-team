ee65c35b7d7f49036fc2560265cad9b04bd8229c
Demorey <demorey2@gmail.com>
2023-10-25 22:51:04 +0300

    #140 add ProjectTaskCreateArgs

---
+++ features/project/server/Garnet.Projects.Application/ProjectTask/Args/ProjectCreateArgs.cs
@@ -0,0 +1,11 @@
+﻿namespace Garnet.Projects.Application.ProjectTask.Args;
+
+public record ProjectTaskCreateArgs(
+    string ProjectId,
+    string Name,
+    string? Description,
+    string Status,
+    string? TeamExecutorId,
+    string? UserExecutorId,
+    string[] Tags
+    );
