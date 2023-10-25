8befd153e8ed4af59db65b6a5bd82407f588a2cd
Demorey <demorey2@gmail.com>
2023-10-25 22:47:02 +0300

    #140 add test steps

---
+++ features/project/server/Garnet.Projects.AcceptanceTests/Features/ProjectTaskCreate/ProjectTaskCreateSteps.cs
@@ -0,0 +1,30 @@
+﻿using Garnet.Common.AcceptanceTests.Fakes;
+using TechTalk.SpecFlow;
+
+namespace Garnet.Projects.AcceptanceTests.Features.ProjectTaskCreate;
+
+[Binding]
+public class ProjectTaskCreateSteps : BaseSteps
+{
+    private readonly CurrentUserProviderFake _currentUserProviderFake;
+
+    public ProjectTaskCreateSteps(StepsArgs args, CurrentUserProviderFake currentUserProviderFake) : base(args)
+    {
+        _currentUserProviderFake = currentUserProviderFake;
+    }
+
+    [Given(@"пользователь '(.*)' является участником команды '(.*)'")]
+    public async Task ThenПользовательЯвляетсяУчастникомПроекта(string username, string projectName)
+    {
+    }
+
+    [When(@"пользователь '(.*)' создает в проекте '(.*)' задачу с названием '(.*)'")]
+    public async Task WhenПользовательСоздаетВПроектеЗадачу(string username, string projectName, string taskName)
+    {
+    }
+
+    [Then(@"в системе существует задача с названием '(.*)'")]
+    public async Task ThenВСистемеСуществуетЗадача(string taskName)
+    {
+    }
+}
