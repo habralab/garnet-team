4a84378e3f637dcce3ba8b6ea600fecd049ae5f4
gfg7 <nakonechnaya040702@gmail.com>
2023-10-27 02:11:49 +0500

    #152 add support notification builder

---
+++ features/notification/server/Garnet.Notifications.AcceptanceTests/Support/NotificationDocumentBuilder.cs
@@ -0,0 +1,65 @@
+using Garnet.Common.AcceptanceTests.Support;
+using Garnet.Common.Infrastructure.Support;
+using Garnet.Notifications.Infrastructure.MongoDB;
+
+namespace Garnet.Notifications.AcceptanceTests.Support
+{
+    public class NotificationDocumentBuilder
+    {
+        private string _id = Uuid.NewMongo();
+        private string _title = "Title";
+        private string _body = "Body";
+        private DateTimeOffset _createdAt;
+        private string _userId = "system";
+        private string _type = "Type";
+        private string? _linkedEntityId;
+
+        public NotificationDocumentBuilder WithTitle(string title)
+        {
+            _title = title;
+            return this;
+        }
+
+        public NotificationDocumentBuilder WithBody(string body)
+        {
+            _body = body;
+            return this;
+        }
+
+        public NotificationDocumentBuilder WithUserId(string userId)
+        {
+            _userId = userId;
+            return this;
+        }
+
+        public NotificationDocumentBuilder WithType(string type)
+        {
+            _type = type;
+            return this;
+        }
+
+        public NotificationDocumentBuilder WithLinkedEntityId(string linkedEntityId)
+        {
+            _linkedEntityId = linkedEntityId;
+            return this;
+        }
+
+        public NotificationDocument Build()
+        {
+            return NotificationDocument.Create(_id, _title, _body, _userId, _type, _createdAt, _linkedEntityId);
+        }
+
+        public static implicit operator NotificationDocument(NotificationDocumentBuilder builder)
+        {
+            return builder.Build();
+        }
+    }
+
+    public static class GiveMeExtensions
+    {
+        public static NotificationDocumentBuilder User(this GiveMe _)
+        {
+            return new NotificationDocumentBuilder();
+        }
+    }
+}
--- features/notification/server/Garnet.Notifications.Infrastructure/MongoDB/NotificationDocument.cs
+++ features/notification/server/Garnet.Notifications.Infrastructure/MongoDB/NotificationDocument.cs
@@ -5,8 +5,23 @@ namespace Garnet.Notifications.Infrastructure.MongoDB
         public string Id { get; init; } = null!;
         public string Title { get; init; } = null!;
         public string Body { get; init; } = null!;
+        public DateTimeOffset CreatedAt { get; init; }
         public string UserId { get; init; } = null!;
         public string Type { get; init; } = null!;
         public string? LinkedEntityId { get; init; }
+
+        public static NotificationDocument Create(string id, string title, string body, string userId, string type, DateTimeOffset createdAt, string? linkedEntityId)
+        {
+            return new NotificationDocument()
+            {
+                Id = id,
+                Title = title,
+                Body = body,
+                UserId = userId,
+                Type = type,
+                CreatedAt = createdAt,
+                LinkedEntityId = linkedEntityId
+            };
+        }
     }
 }
