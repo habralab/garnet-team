8958454373d4876605173954a24b461da3e49bf7
gfg7 <nakonechnaya040702@gmail.com>
2023-10-27 02:43:54 +0500

    #152 init test

---
+++ features/notification/server/Garnet.Notifications.AcceptanceTests/Features/NotificationGet/NotificationGetSteps.cs
@@ -0,0 +1,10 @@
+namespace Garnet.Notifications.AcceptanceTests.Features.NotificationGet
+{
+    [Binding]
+    public class NotificationGetSteps : BaseSteps
+    {
+        public NotificationGetSteps(StepsArgs args) : base(args)
+        {
+        }
+    }
+}
