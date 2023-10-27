3e44e9d3f0b5ddd11b3ea98c8a73126d9c8647e8
Dmitry <dimafomin02@gmail.com>
2023-10-27 03:55:57 +0300

    refactor: added HourglassIcon to replacements

--- ui/icon/replacements.ts
+++ ui/icon/replacements.ts
@@ -8,4 +8,5 @@ export const replacements = {
   SearchIcon: getColorReplacement({ color: '#353535', themePaths: ['text'] }),
   UserGroupIcon: getColorReplacement({ color: '#353535', themePaths: ['text'] }),
   UserIcon: getColorReplacement({ color: '#353535', themePaths: ['text'] }),
+  HourglassIcon: getColorReplacement({ color: '#353535', themePaths: ['text'] }),
 }
