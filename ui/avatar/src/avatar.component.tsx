7c186c325c4b5dcc6946d74c7d8f7c7cef8830bc
Dmitry <dimafomin02@gmail.com>
2023-10-27 04:35:49 +0300

    refactor: switched to using Link instead of NextLink

--- ui/avatar/src/avatar.component.tsx
+++ ui/avatar/src/avatar.component.tsx
@@ -3,32 +3,43 @@ import { AvatarImage }          from '@atls-ui-proto/avatar'
 
 import React                    from 'react'
 import { FC }                   from 'react'
+import { useRouter }            from 'next/router'
 
 import { Condition }            from '@ui/condition'
 import { Box }                  from '@ui/layout'
-import { NextLink }             from '@ui/link'
+import { Link }                 from '@ui/link'
 import { theme }                from '@ui/theme'
 
 import { AvatarProps }          from './avatar.interfaces'
 
-export const Avatar: FC<AvatarProps> = ({ image, shape = 'circle', size = 74, url, title }) => (
-  <Box title={title} style={{ cursor: url ? 'pointer' : 'default' }}>
-    <AvatarRoot
-      style={{
-        width: size,
-        height: size,
-        backgroundColor: 'transparent',
-        borderRadius: shape === 'circle' ? theme.radii.full : theme.radii.medium,
-      }}
-    >
-      <Condition match={Boolean(url)}>
-        <NextLink path={url} href={url} width='100%' height='100%'>
+export const Avatar: FC<AvatarProps> = ({ image, shape = 'circle', size = 74, url, title }) => {
+  const router = useRouter()
+
+  const handleClick = (event) => {
+    event.preventDefault()
+
+    if (url) router.push(url)
+  }
+
+  return (
+    <Box title={title} style={{ cursor: url ? 'pointer' : 'default' }}>
+      <AvatarRoot
+        style={{
+          width: size,
+          height: size,
+          backgroundColor: 'transparent',
+          borderRadius: shape === 'circle' ? theme.radii.full : theme.radii.medium,
+        }}
+      >
+        <Condition match={Boolean(url)}>
+          <Link href={url} width='100%' height='100%' onClick={handleClick}>
+            <AvatarImage src={image} />
+          </Link>
+        </Condition>
+        <Condition match={!url}>
           <AvatarImage src={image} />
-        </NextLink>
-      </Condition>
-      <Condition match={!url}>
-        <AvatarImage src={image} />
-      </Condition>
-    </AvatarRoot>
-  </Box>
-)
+        </Condition>
+      </AvatarRoot>
+    </Box>
+  )
+}
