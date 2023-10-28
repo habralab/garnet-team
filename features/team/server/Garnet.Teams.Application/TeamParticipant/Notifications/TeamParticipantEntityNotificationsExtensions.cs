2fdbc05c7509704d13e1ec7551636bb4d9b8067d
gfg7 <nakonechnaya040702@gmail.com>
2023-10-29 02:28:34 +0500

    #171 add team participant left notice

---
+++ features/team/server/Garnet.Teams.Application/TeamParticipant/Notifications/TeamParticipantEntityNotificationsExtensions.cs
@@ -0,0 +1,20 @@
+using Garnet.Notifications.Events;
+using Garnet.Teams.Application.Team;
+
+namespace Garnet.Teams.Application.TeamParticipant.Notifications
+{
+    public static class TeamParticipantEntityNotificationsExtensions
+    {
+        public static SendNotificationCommandMessage CreateParticipantLeaveTeamNotification(this TeamParticipantEntity participant, TeamEntity team, string username)
+        {
+            return new SendNotificationCommandMessage(
+                Title: "Участник покинул команду",
+                Body: $"Пользователь {username} вышел из состава команды {team.Name}",
+                team.OwnerUserId,
+                Type: "TeamInvite",
+                DateTimeOffset.Now,
+                participant.UserId
+            );
+        }
+    }
+}
