96cff44dbe4704ff9ed0c6d81e24c92c6eb2bcf8
gfg7 <nakonechnaya040702@gmail.com>
2023-10-29 02:16:39 +0500

    #168 add notification to command

--- features/team/server/Garnet.Teams.Application/TeamUserJoinRequest/Commands/TeamUserJoinRequestCancelCommand.cs
+++ features/team/server/Garnet.Teams.Application/TeamUserJoinRequest/Commands/TeamUserJoinRequestCancelCommand.cs
@@ -1,7 +1,10 @@
 using FluentResults;
 using Garnet.Common.Application;
 using Garnet.Common.Application.MessageBus;
+using Garnet.Teams.Application.Team;
+using Garnet.Teams.Application.TeamUser;
 using Garnet.Teams.Application.TeamUserJoinRequest.Errors;
+using Garnet.Teams.Application.TeamUserJoinRequest.Notifications;
 
 namespace Garnet.Teams.Application.TeamUserJoinRequest.Commands
 {
@@ -9,15 +12,21 @@ namespace Garnet.Teams.Application.TeamUserJoinRequest.Commands
     {
         private readonly ICurrentUserProvider _currentUserProvider;
         private readonly ITeamUserJoinRequestRepository _teamUserJoinRequestRepository;
+        private readonly ITeamUserRepository _teamUserRepository;
+        private readonly ITeamRepository _teamRepository;
         private readonly IMessageBus _messageBus;
         public TeamUserJoinRequestCancelCommand(
             IMessageBus messageBus,
+            ITeamRepository teamRepository,
+            ITeamUserRepository teamUserRepository,
             ICurrentUserProvider currentUserProvider,
             ITeamUserJoinRequestRepository teamUserJoinRequestRepository)
         {
             _currentUserProvider = currentUserProvider;
             _teamUserJoinRequestRepository = teamUserJoinRequestRepository;
             _messageBus = messageBus;
+            _teamRepository = teamRepository;
+            _teamUserRepository = teamUserRepository;
         }
 
         public async Task<Result<TeamUserJoinRequestEntity>> Execute(CancellationToken ct, string userJoinRequestId)
@@ -37,6 +46,11 @@ namespace Garnet.Teams.Application.TeamUserJoinRequest.Commands
 
             var @event = userJoinRequest.ToCancelledEvent();
             await _messageBus.Publish(@event);
+
+            var team = await _teamRepository.GetTeamById(ct, userJoinRequest.TeamId);
+            var user = await _teamUserRepository.GetUser(ct, userJoinRequest.UserId);
+            var notification = userJoinRequest.CreateTeamUserJoinRequestCancelNotification(team!, user!.Username);
+            await _messageBus.Publish(notification);
             return Result.Ok(userJoinRequest);
         }
     }
