2dc633fe3d7f6a3ba466ce4af0157c76cd31dac0
Demorey <demorey2@gmail.com>
2023-10-29 23:21:24 +0300

    #156 add delete task command

---
+++ features/project/server/Garnet.Projects.Application/ProjectTask/Commands/ProjectTaskDeleteCommand.cs
@@ -0,0 +1,60 @@
+﻿using FluentResults;
+using Garnet.Common.Application;
+using Garnet.Common.Application.MessageBus;
+using Garnet.Projects.Application.Project;
+using Garnet.Projects.Application.Project.Errors;
+using Garnet.Projects.Application.ProjectTask.Errors;
+using Garnet.Projects.Application.ProjectTeamParticipant;
+
+namespace Garnet.Projects.Application.ProjectTask.Commands;
+
+public class ProjectTaskDeleteCommand
+{
+    private readonly ICurrentUserProvider _currentUserProvider;
+    private readonly IProjectRepository _projectRepository;
+    private readonly IProjectTeamParticipantRepository _projectTeamParticipantRepository;
+    private readonly IProjectTaskRepository _projectTaskRepository;
+    private readonly IMessageBus _messageBus;
+
+    public ProjectTaskDeleteCommand(
+        ICurrentUserProvider currentUserProvider,
+        IProjectRepository projectRepository,
+        IProjectTeamParticipantRepository projectTeamParticipantRepository,
+        IMessageBus messageBus, IProjectTaskRepository projectTaskRepository)
+    {
+        _currentUserProvider = currentUserProvider;
+        _projectRepository = projectRepository;
+        _projectTeamParticipantRepository = projectTeamParticipantRepository;
+        _messageBus = messageBus;
+        _projectTaskRepository = projectTaskRepository;
+    }
+
+    public async Task<Result<ProjectTaskEntity>> Execute(CancellationToken ct, string projectId, string taskId)
+    {
+        var currentUserId = _currentUserProvider.UserId;
+        var project = await _projectRepository.GetProject(ct, projectId);
+        if (project is null)
+        {
+            return Result.Fail(new ProjectNotFoundError(projectId));
+        }
+
+        var task = await _projectTaskRepository.GetProjectTaskById(ct, taskId);
+        if (task is null)
+        {
+            return Result.Fail(new ProjectTaskNotFoundError(taskId));
+        }
+
+        var teamParticipants =
+            await _projectTeamParticipantRepository.GetProjectTeamParticipantsByProjectId(ct, project.Id);
+        var user = teamParticipants.FirstOrDefault(x => x.UserParticipants.Any(
+            o => o.Id == currentUserId));
+        if (user is null)
+        {
+            return Result.Fail(new ProjectOnlyParticipantCanDeleteTaskError());
+        }
+
+        task = await _projectTaskRepository.DeleteProjectTask(ct, taskId);
+        await _messageBus.Publish(task.ToDeletedEvent());
+        return Result.Ok(task);
+    }
+}
