9e7870ad055766676f8c4b4ae171e8e2454ef511
Demorey <demorey2@gmail.com>
2023-10-29 14:54:21 +0300

    #180 add payload

---
+++ features/project/server/Garnet.Projects.Infrastructure/Api/ProjectTaskGet/ProjectTaskGetPayload.cs
@@ -0,0 +1,15 @@
+﻿namespace Garnet.Projects.Infrastructure.Api.ProjectTaskGet;
+
+public record ProjectTaskGetPayload(
+    string Id,
+    int TaskNumber,
+    string ProjectId,
+    string ResponsibleUserId,
+    string Name,
+    string? Description,
+    string Status,
+    string[] TeamExecutorIds,
+    string[] UserExecutorIds,
+    string[] Tags,
+    string[] Labels
+);
