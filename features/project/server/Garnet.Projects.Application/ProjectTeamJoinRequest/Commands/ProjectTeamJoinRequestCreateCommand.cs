ce1cec978aa742e16dccab1570cb2825a4dcef9d
gfg7 <nakonechnaya040702@gmail.com>
2023-10-30 00:57:21 +0500

    #174 add error

--- features/project/server/Garnet.Projects.Application/ProjectTeamJoinRequest/Commands/ProjectTeamJoinRequestCreateCommand.cs
+++ features/project/server/Garnet.Projects.Application/ProjectTeamJoinRequest/Commands/ProjectTeamJoinRequestCreateCommand.cs
@@ -1,5 +1,7 @@
-﻿using Garnet.Common.Application.MessageBus;
+﻿using FluentResults;
+using Garnet.Common.Application.MessageBus;
 using Garnet.Projects.Application.Project;
+using Garnet.Projects.Application.Project.Errors;
 using Garnet.Projects.Application.ProjectTeamJoinRequest.Notifications;
 
 namespace Garnet.Projects.Application.ProjectTeamJoinRequest.Commands;
@@ -20,18 +22,19 @@ public class ProjectTeamJoinRequestCreateCommand
         _messageBus = messageBus;
     }
 
-    public async Task Execute(CancellationToken ct, string teamId,
+    public async Task<Result<ProjectTeamJoinRequestEntity>> Execute(CancellationToken ct, string teamId,
         string teamName,
         string projectId)
     {
         var project = await _projectRepository.GetProject(ct, projectId);
         if (project is null)
         {
-            return;
+            return Result.Fail(new ProjectNotFoundError(projectId));
         }
 
         var request = await _projectTeamJoinRequestRepository.AddProjectTeamJoinRequest(ct, teamId, teamName, projectId);
         var notification = request.CreateProjectTeamJoinRequestNotification(project!);
         await _messageBus.Publish(notification);
+        return Result.Ok(request);
     }
 }
