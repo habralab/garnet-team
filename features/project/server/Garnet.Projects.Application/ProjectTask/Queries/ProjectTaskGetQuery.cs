782afedb07e0e643653ce679df76faa0193dd550
Demorey <demorey2@gmail.com>
2023-10-29 14:54:48 +0300

    #180 add get task query

---
+++ features/project/server/Garnet.Projects.Application/ProjectTask/Queries/ProjectTaskGetQuery.cs
@@ -0,0 +1,20 @@
+﻿using FluentResults;
+using Garnet.Projects.Application.ProjectTask.Errors;
+
+namespace Garnet.Projects.Application.ProjectTask.Queries;
+
+public class ProjectTaskGetQuery
+{
+    private readonly IProjectTaskRepository _projectTaskRepository;
+
+    public ProjectTaskGetQuery(IProjectTaskRepository projectTaskRepository)
+    {
+        _projectTaskRepository = projectTaskRepository;
+    }
+
+    public async Task<Result<ProjectTaskEntity>> Query(CancellationToken ct, string taskId)
+    {
+        var task = await _projectTaskRepository.GetProjectTaskById(ct, taskId);
+        return task is null ? Result.Fail(new ProjectTaskNotFoundError(taskId)) : Result.Ok(task);
+    }
+}
