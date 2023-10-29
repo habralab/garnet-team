5ea6d3ab9ca93982050373f3c26b1e88e8be3d84
gfg7 <nakonechnaya040702@gmail.com>
2023-10-30 00:02:15 +0500

    #174 add notification to command

--- features/project/server/Garnet.Projects.Application/ProjectTeamJoinRequest/Commands/ProjectTeamJoinRequestCreateCommand.cs
+++ features/project/server/Garnet.Projects.Application/ProjectTeamJoinRequest/Commands/ProjectTeamJoinRequestCreateCommand.cs
@@ -1,18 +1,37 @@
-﻿namespace Garnet.Projects.Application.ProjectTeamJoinRequest.Commands;
+﻿using Garnet.Common.Application.MessageBus;
+using Garnet.Projects.Application.Project;
+using Garnet.Projects.Application.ProjectTeamJoinRequest.Notifications;
+
+namespace Garnet.Projects.Application.ProjectTeamJoinRequest.Commands;
 
 public class ProjectTeamJoinRequestCreateCommand
 {
     private readonly IProjectTeamJoinRequestRepository _projectTeamJoinRequestRepository;
+    private readonly IProjectRepository _projectRepository;
+    private readonly IMessageBus _messageBus;
 
-    public ProjectTeamJoinRequestCreateCommand(IProjectTeamJoinRequestRepository projectTeamJoinRequestRepository)
+    public ProjectTeamJoinRequestCreateCommand(
+        IProjectRepository projectRepository,
+        IProjectTeamJoinRequestRepository projectTeamJoinRequestRepository,
+        IMessageBus messageBus)
     {
         _projectTeamJoinRequestRepository = projectTeamJoinRequestRepository;
+        _projectRepository = projectRepository;
+        _messageBus = messageBus;
     }
 
-    public async Task<ProjectTeamJoinRequestEntity> Execute(CancellationToken ct, string teamId,
+    public async Task Execute(CancellationToken ct, string teamId,
         string teamName,
         string projectId)
     {
-        return await _projectTeamJoinRequestRepository.AddProjectTeamJoinRequest(ct, teamId, teamName, projectId);
+        var project = await _projectRepository.GetProject(ct, projectId);
+        if (project is null)
+        {
+            return;
+        }
+
+        var request = await _projectTeamJoinRequestRepository.AddProjectTeamJoinRequest(ct, teamId, teamName, projectId);
+        var notification = request.CreateProjectTeamJoinRequestNotification(project!);
+        await _messageBus.Publish(notification);
     }
 }
