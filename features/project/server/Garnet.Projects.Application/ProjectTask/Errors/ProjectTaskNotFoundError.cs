0eb18618bc46c1fb6ab199ce18e53f97205f9c8f
Demorey <demorey2@gmail.com>
2023-10-29 14:53:13 +0300

    #180 add error

---
+++ features/project/server/Garnet.Projects.Application/ProjectTask/Errors/ProjectTaskNotFoundError.cs
@@ -0,0 +1,13 @@
+﻿using Garnet.Common.Application.Errors;
+
+namespace Garnet.Projects.Application.ProjectTask.Errors;
+
+public class ProjectTaskNotFoundError : ApplicationError
+{
+    public ProjectTaskNotFoundError(string taskId)
+        : base($"Задача с идентификатором '{taskId}' не найдена")
+    {
+    }
+
+    public override string Code => nameof(ProjectTaskNotFoundError);
+}
