6f4eb719c56db78003e3de8342e83f0cf32e1d2b
Demorey <demorey2@gmail.com>
2023-10-31 20:41:46 +0300

    #193 add EditProjectTaskLabels method

--- features/project/server/Garnet.Projects.Infrastructure/MongoDb/ProjectTask/ProjectTaskRepository.cs
+++ features/project/server/Garnet.Projects.Infrastructure/MongoDb/ProjectTask/ProjectTaskRepository.cs
@@ -81,11 +81,29 @@ public class ProjectTaskRepository : RepositoryBase, IProjectTaskRepository
 
         return ProjectTaskDocument.ToDomain(task);
     }
-    public async Task<ProjectTaskEntity> EditProjectTaskDescription(CancellationToken ct, string taskId, string description)
+
+    public async Task<ProjectTaskEntity> EditProjectTaskDescription(CancellationToken ct, string taskId,
+        string description)
     {
         var db = _dbFactory.Create();
         var filter = _f.Eq(x => x.Id, taskId);
         var update = _u.Set(x => x.Description, description);
+
+        var task = await FindOneAndUpdateDocument(
+            ct,
+            db.ProjectTasks,
+            filter,
+            update
+        );
+
+        return ProjectTaskDocument.ToDomain(task);
+    }
+
+    public async Task<ProjectTaskEntity> EditProjectTaskLabels(CancellationToken ct, string taskId, string[] labels)
+    {
+        var db = _dbFactory.Create();
+        var filter = _f.Eq(x => x.Id, taskId);
+        var update = _u.Set(x => x.Labels, labels);
 
         var task = await FindOneAndUpdateDocument(
             ct,
