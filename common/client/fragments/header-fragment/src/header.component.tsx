86710fa833bd229a6bf3f5e2f7fcc03ec86c55a9
Dmitry <dimafomin02@gmail.com>
2023-10-31 13:56:18 +0300

    feat: added disable navigation for header

--- common/client/fragments/header-fragment/src/header.component.tsx
+++ common/client/fragments/header-fragment/src/header.component.tsx
@@ -5,6 +5,7 @@ import { FormattedMessage } from 'react-intl'
 import { useRouter }        from 'next/router'
 
 import { Background }       from '@ui/background'
+import { Condition }        from '@ui/condition'
 import { Layout }           from '@ui/layout'
 import { Row }              from '@ui/layout'
 import { Column }           from '@ui/layout'
@@ -13,10 +14,11 @@ import { NextLink }         from '@ui/link'
 import { Logo }             from '@ui/logo'
 
 import { ButtonExit }       from './button-exit'
+import { HeaderProps }      from './header.interfaces'
 import { TabItem }          from './tab-item'
 import { navigationItems }  from './header.constants'
 
-export const Header: FC = () => {
+export const Header: FC<HeaderProps> = ({ disableNavigation = false }) => {
   const { pathname } = useRouter()
 
   const currentPathname = `/${pathname.split('/')[1]}`
@@ -26,22 +28,26 @@ export const Header: FC = () => {
       <Column fill>
         <Layout flexBasis={36.5} flexShrink={0} />
         <Row justifyContent='space-between' alignItems='center'>
-          <Layout flexBasis={24} flexShrink={0} />
-          <Box minWidth={202} height={47}>
-            <Logo />
+          <Box height={47}>
+            <Layout flexBasis={24} flexShrink={0} />
+            <Box minWidth={202}>
+              <Logo withLink={!disableNavigation} />
+            </Box>
           </Box>
-          <Layout flexBasis={100} flexShrink={0} />
-          {navigationItems.map(({ id, url }) => (
-            <Fragment key={id}>
-              <NextLink path={url} href={url} active>
-                <TabItem active={url === currentPathname}>
-                  <FormattedMessage id={id} />
-                </TabItem>
-              </NextLink>
-              <Layout flexBasis={30} flexShrink={0} />
-            </Fragment>
-          ))}
-          <ButtonExit />
+          <Condition match={!disableNavigation}>
+            <Layout flexBasis={100} flexShrink={0} />
+            {navigationItems.map(({ id, url }) => (
+              <Fragment key={id}>
+                <NextLink path={url} href={url} active>
+                  <TabItem active={url === currentPathname}>
+                    <FormattedMessage id={id} />
+                  </TabItem>
+                </NextLink>
+                <Layout flexBasis={30} flexShrink={0} />
+              </Fragment>
+            ))}
+            <ButtonExit />
+          </Condition>
         </Row>
         <Layout flexBasis={36.5} flexShrink={0} />
       </Column>
