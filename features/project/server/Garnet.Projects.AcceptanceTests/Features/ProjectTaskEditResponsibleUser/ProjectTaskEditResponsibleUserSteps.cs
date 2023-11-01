c4d82eac295062c4bd8fede31f0d2d5afa8b7a6c
Demorey <demorey2@gmail.com>
2023-11-01 15:12:34 +0300

    #196 add test steps

---
+++ features/project/server/Garnet.Projects.AcceptanceTests/Features/ProjectTaskEditResponsibleUser/ProjectTaskEditResponsibleUserSteps.cs
@@ -0,0 +1,55 @@
+﻿using FluentAssertions;
+using Garnet.Common.AcceptanceTests.Contexts;
+using Garnet.Common.AcceptanceTests.Fakes;
+using Garnet.Projects.Infrastructure.MongoDb.ProjectTask;
+using HotChocolate.Execution;
+using MongoDB.Driver;
+using TechTalk.SpecFlow;
+
+namespace Garnet.Projects.AcceptanceTests.Features.ProjectTaskEditResponsibleUser;
+
+[Binding]
+public class ProjectTaskEditResponsibleUserSteps : BaseSteps
+{
+    private readonly CurrentUserProviderFake _currentUserProviderFake;
+    private QueryExceptionsContext _errorStepContext;
+
+    private readonly FilterDefinitionBuilder<ProjectTaskDocument> _f =
+        Builders<ProjectTaskDocument>.Filter;
+
+    private readonly UpdateDefinitionBuilder<ProjectTaskDocument> _u =
+        Builders<ProjectTaskDocument>.Update;
+
+    public ProjectTaskEditResponsibleUserSteps(StepsArgs args,
+        CurrentUserProviderFake currentUserProviderFake,
+        QueryExceptionsContext errorStepContext) : base(args)
+    {
+        _currentUserProviderFake = currentUserProviderFake;
+        _errorStepContext = errorStepContext;
+    }
+
+    [When(@"пользователь '(.*)' меняет отвественного по задаче '(.*)' на пользователя '(.*)'")]
+    public async Task WhenПользовательМеняетОтветственногоЗадачи(string username, string taskName, string newResponsibleUserName)
+    {
+        _currentUserProviderFake.LoginAs(username);
+        var task = await Db.ProjectTasks.Find(o => o.Name == taskName).FirstAsync();
+        var user = await Db.ProjectUsers.Find(o => o.UserName == newResponsibleUserName).FirstAsync();
+
+        try
+        {
+            await Mutation.ProjectTaskEditResponsibleUser(CancellationToken.None, task.Id, user.Id);
+        }
+        catch (QueryException ex)
+        {
+            _errorStepContext.QueryExceptions.Add(ex);
+        }
+    }
+
+    [Then(@"отвественным по задаче '(.*)' является пользователь '(.*)'")]
+    public async Task ThenОтвественнымЗадачиЯвляется(string taskName, string responsibleUserName)
+    {
+        var user = await Db.ProjectUsers.Find(x => x.UserName == responsibleUserName).FirstOrDefaultAsync();
+        var task = await Db.ProjectTasks.Find(x => x.Name == taskName).FirstOrDefaultAsync();
+        task.ResponsibleUserId.Should().BeEquivalentTo(user.Id);
+    }
+}
