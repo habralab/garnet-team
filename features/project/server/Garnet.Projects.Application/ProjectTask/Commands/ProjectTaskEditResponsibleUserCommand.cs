290033207d233b669feadc27c82c2c3c35446a1d
Demorey <demorey2@gmail.com>
2023-11-01 16:21:18 +0300

    #196 add ProjectTaskParticipantOnlyCanSetAsResponsibleUserError

--- features/project/server/Garnet.Projects.Application/ProjectTask/Commands/ProjectTaskEditResponsibleUserCommand.cs
+++ features/project/server/Garnet.Projects.Application/ProjectTask/Commands/ProjectTaskEditResponsibleUserCommand.cs
@@ -4,6 +4,7 @@ using Garnet.Common.Application.MessageBus;
 using Garnet.Projects.Application.Project;
 using Garnet.Projects.Application.Project.Errors;
 using Garnet.Projects.Application.ProjectTask.Errors;
+using Garnet.Projects.Application.ProjectTeamParticipant;
 
 namespace Garnet.Projects.Application.ProjectTask.Commands;
 
@@ -11,22 +12,26 @@ public class ProjectTaskEditResponsibleUserCommand
 {
     private readonly ICurrentUserProvider _currentUserProvider;
     private readonly IProjectTaskRepository _projectTaskRepository;
+    private readonly IProjectTeamParticipantRepository _projectTeamParticipantRepository;
     private readonly IProjectRepository _projectRepository;
     private readonly IMessageBus _messageBus;
 
     public ProjectTaskEditResponsibleUserCommand(
         ICurrentUserProvider currentUserProvider,
         IProjectTaskRepository projectTaskRepository,
+        IProjectTeamParticipantRepository projectTeamParticipantRepository,
         IMessageBus messageBus,
         IProjectRepository projectRepository)
     {
         _currentUserProvider = currentUserProvider;
         _projectTaskRepository = projectTaskRepository;
+        _projectTeamParticipantRepository = projectTeamParticipantRepository;
         _messageBus = messageBus;
         _projectRepository = projectRepository;
     }
 
-    public async Task<Result<ProjectTaskEntity>> Execute(CancellationToken ct, string taskId, string newResponsibleUserId)
+    public async Task<Result<ProjectTaskEntity>> Execute(CancellationToken ct, string taskId,
+        string newResponsibleUserId)
     {
         var currentUserId = _currentUserProvider.UserId;
 
@@ -50,6 +55,15 @@ public class ProjectTaskEditResponsibleUserCommand
         if (currentUserId != task.ResponsibleUserId && currentUserId != project.OwnerUserId)
         {
             return Result.Fail(new ProjectTaskResponsiblePersonOnlyCanEditResponsibleUserError());
+        }
+
+        var teamParticipants =
+            await _projectTeamParticipantRepository.GetProjectTeamParticipantsByProjectId(ct, project.Id);
+        var user = teamParticipants.FirstOrDefault(x => x.UserParticipants.Any(
+            o => o.Id == newResponsibleUserId));
+        if (user is null)
+        {
+            return Result.Fail(new ProjectTaskParticipantOnlyCanSetAsResponsibleUserError());
         }
 
         task = await _projectTaskRepository.EditProjectTaskResponsibleUser(ct, taskId, newResponsibleUserId);
---
+++ features/project/server/Garnet.Projects.Application/ProjectTask/Errors/ProjectTaskParticipantOnlyCanSetAsResponsibleUserError.cs
@@ -0,0 +1,12 @@
+﻿using Garnet.Common.Application.Errors;
+
+namespace Garnet.Projects.Application.ProjectTask.Errors;
+
+public class ProjectTaskParticipantOnlyCanSetAsResponsibleUserError : ApplicationError
+{
+    public ProjectTaskParticipantOnlyCanSetAsResponsibleUserError() : base("Только участник проекта может быть выбран отвественным за задачу")
+    {
+    }
+
+    public override string Code => nameof(ProjectTaskParticipantOnlyCanSetAsResponsibleUserError);
+}
