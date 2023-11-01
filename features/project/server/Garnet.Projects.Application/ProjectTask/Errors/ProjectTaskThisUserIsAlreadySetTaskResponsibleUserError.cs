fc50d58771cb2098186473246a5429c2cc480fdd
Demorey <demorey2@gmail.com>
2023-11-01 15:13:13 +0300

    #196 add error

---
+++ features/project/server/Garnet.Projects.Application/ProjectTask/Errors/ProjectTaskThisUserIsAlreadySetTaskResponsibleUserError.cs
@@ -0,0 +1,13 @@
+﻿using Garnet.Common.Application.Errors;
+
+namespace Garnet.Projects.Application.ProjectTask.Errors;
+
+public class ProjectTaskThisUserIsAlreadySetTaskResponsibleUserError : ApplicationError
+{
+    public ProjectTaskThisUserIsAlreadySetTaskResponsibleUserError() : base(
+        "Этот пользователь уже является отвественным за задачу")
+    {
+    }
+
+    public override string Code => nameof(ProjectTaskThisUserIsAlreadySetTaskResponsibleUserError);
+}
