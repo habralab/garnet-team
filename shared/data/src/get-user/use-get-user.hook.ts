d58a8381e461426166351ec4d29d4843bb7d4b60
Dmitry <dimafomin02@gmail.com>
2023-11-02 18:34:56 +0300

    feat: added get-user query

---
+++ shared/data/src/get-user/use-get-user.hook.ts
@@ -0,0 +1,26 @@
+import { useQuery }        from '@apollo/client'
+
+import { FilterVariables } from '@shared/data'
+import { Team }            from '@shared/data'
+import { User }            from '@shared/data'
+
+import { GET_USER }        from './get-user.query'
+
+export interface GetUserResponse {
+  userGet: User
+  teamsListByUser: {
+    teams: Team[]
+  }
+}
+
+export interface GetUserInput extends Partial<FilterVariables> {
+  id: User['id']
+}
+
+export const useGetUser = (variables: GetUserInput) => {
+  const { data, refetch } = useQuery<GetUserResponse, GetUserInput>(GET_USER, {
+    variables,
+  })
+
+  return { user: data?.userGet, teams: data?.teamsListByUser.teams || [], refetch }
+}
