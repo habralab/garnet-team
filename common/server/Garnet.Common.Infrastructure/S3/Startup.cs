95b61ce3b938f406e982b9651f5c6c37fc45ffa4
picolino <webmaster.pico@gmail.com>
2023-11-02 20:28:12 +0300

    #207: Fix second requests to s3 storage api

--- common/server/Garnet.Common.Infrastructure/S3/Startup.cs
+++ common/server/Garnet.Common.Infrastructure/S3/Startup.cs
@@ -13,9 +13,21 @@ public static class Startup
     {
         services.AddMinio(o =>
             {
+                // Господи спаси и сохрани разработчиков библиотеки minio-dotnet
+                // которые не сумели сделать на ПРОСТЕЙШЕМ функционале
+                // корректную регистрацию scope-объектов
+                // из-за чего их объект-клиент живет как signleton всегда и это поведение
+                // невозможно переопределить. А единственное место, где можно отключить dispose
+                // используемого внутри HttpClient - это вызов этого всратого метода ниже.
+                // СПАСИБО, что дали хотя бы такую возможность!
+                
+                o.WithHttpClient(null); // <------ ВОТ ОН
+                
                 o.WithEndpoint(EnvironmentEx.GetRequiredEnvironmentVariable("S3_ENDPOINT"));
-                o.WithCredentials(EnvironmentEx.GetRequiredEnvironmentVariable("S3_ACCESS_KEY"),
-                    EnvironmentEx.GetRequiredEnvironmentVariable("S3_SECRET_KEY"));
+                o.WithCredentials(
+                    EnvironmentEx.GetRequiredEnvironmentVariable("S3_ACCESS_KEY"),
+                    EnvironmentEx.GetRequiredEnvironmentVariable("S3_SECRET_KEY")
+                );
                 o.Build();
             },
             ServiceLifetime.Scoped
