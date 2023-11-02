09db47aeb6d105b626a485a40166cd8e4caff9d2
Dmitry <dimafomin02@gmail.com>
2023-11-02 18:37:27 +0300

    feat: added blob prop to onConfirm

--- shared/fragments/modals/src/modal-edit-image/modal-edit-image.interfaces.ts
+++ shared/fragments/modals/src/modal-edit-image/modal-edit-image.interfaces.ts
@@ -3,6 +3,6 @@ import { ContainerProps } from './container'
 export interface ModalEditImageProps extends ContainerProps {
   modalOpen?: boolean
   onClose?: () => void
-  onConfirm?: (url?: string) => void
+  onConfirm?: (url?: string, blob?: Blob) => void
   image?: string
 }
