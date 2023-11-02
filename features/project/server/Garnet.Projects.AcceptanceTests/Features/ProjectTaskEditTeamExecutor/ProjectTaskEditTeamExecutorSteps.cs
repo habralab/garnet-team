8eab9c5df5f70efa8746d0924b72490018bed0d2
Demorey <demorey2@gmail.com>
2023-11-02 15:25:55 +0300

    #209 add test steps

---
+++ features/project/server/Garnet.Projects.AcceptanceTests/Features/ProjectTaskEditTeamExecutor/ProjectTaskEditTeamExecutorSteps.cs
@@ -0,0 +1,78 @@
+﻿using FluentAssertions;
+using Garnet.Common.AcceptanceTests.Contexts;
+using Garnet.Common.AcceptanceTests.Fakes;
+using Garnet.Projects.Infrastructure.Api.ProjectTaskEditTeamExecutor;
+using Garnet.Projects.Infrastructure.MongoDb.ProjectTask;
+using HotChocolate.Execution;
+using MongoDB.Driver;
+using TechTalk.SpecFlow;
+
+namespace Garnet.Projects.AcceptanceTests.Features.ProjectTaskEditTeamExecutor;
+
+[Binding]
+public class ProjectTaskEditTeamExecutorSteps : BaseSteps
+{
+    private readonly CurrentUserProviderFake _currentUserProviderFake;
+    private QueryExceptionsContext _errorStepContext;
+
+    private readonly FilterDefinitionBuilder<ProjectTaskDocument> _f =
+        Builders<ProjectTaskDocument>.Filter;
+
+    private readonly UpdateDefinitionBuilder<ProjectTaskDocument> _u =
+        Builders<ProjectTaskDocument>.Update;
+
+    public ProjectTaskEditTeamExecutorSteps(StepsArgs args, CurrentUserProviderFake currentUserProviderFake,
+        QueryExceptionsContext errorStepContext) : base(args)
+    {
+        _currentUserProviderFake = currentUserProviderFake;
+        _errorStepContext = errorStepContext;
+    }
+
+    [Given(@"команда '(.*)' является исполнителем задачи '(.*)'")]
+    public async Task GivenКомандаЯвляетсяИсполнителемЗадачи(string teamName, string taskName)
+    {
+        var team = await Db.ProjectTeamsParticipants.Find(x => x.TeamName == teamName).FirstAsync();
+        await Db.ProjectTasks.UpdateOneAsync(
+            _f.Eq(x => x.Name, taskName),
+            _u.AddToSet(x => x.TeamExecutorIds, team.TeamId));
+    }
+
+    [When(@"пользователь '(.*)' редактирует команду-исполнителя задачи '(.*)' на список команд '(.*)'")]
+    public async Task WhenПользовательРедактируетКомандуИсполнителя(string username, string taskName, string teams)
+    {
+        _currentUserProviderFake.LoginAs(username);
+        var teamList = teams.Split(", ");
+        var teamIdList = new List<string>();
+        foreach (var teamName in teamList)
+        {
+            var team = await Db.ProjectTeams.Find(o => o.TeamName == teamName).FirstAsync();
+            teamIdList.Add(team.Id);
+        }
+
+        var task = await Db.ProjectTasks.Find(o => o.Name == taskName).FirstAsync();
+        var input = new ProjectTaskEditTeamExecutorInput(task.Id, teamIdList.ToArray());
+        try
+        {
+            await Mutation.ProjectTaskEditTeamExecutor(CancellationToken.None, input);
+        }
+        catch (QueryException ex)
+        {
+            _errorStepContext.QueryExceptions.Add(ex);
+        }
+    }
+
+    [Then(@"командами-исполнителями задачи '(.*)' являются '(.*)'")]
+    public async Task ThenКомандамиИсполнителямиЗадачиЯвляются(string taskName, string teams)
+    {
+        var teamList = teams.Split(", ");
+        var teamIdList = new List<string>();
+        foreach (var teamName in teamList)
+        {
+            var team = await Db.ProjectTeamsParticipants.Find(o => o.TeamName == teamName).FirstAsync();
+            teamIdList.Add(team.TeamId);
+        }
+
+        var task = await Db.ProjectTasks.Find(o => o.Name == taskName).FirstAsync();
+        task.TeamExecutorIds.Should().BeEquivalentTo(teamIdList);
+    }
+}
