a17a2d504170a744f68eab0161b53f72d4a6a5c5
Dmitry <dimafomin02@gmail.com>
2023-11-03 02:35:35 +0300

    refactor: changed name to projectName in Project

--- common/client/fragments/project-profile-fragment/src/profile-description/profile-description.component.tsx
+++ common/client/fragments/project-profile-fragment/src/profile-description/profile-description.component.tsx
@@ -16,7 +16,7 @@ export const ProfileDescription: FC<ProfileDescriptionProps> = ({ project }) => 
   <Row fill flexWrap='wrap' gap={32} justifyContent='space-between'>
     <Column height='max-content' minWidth={300} maxWidth='100%' flex='1 0 47%'>
       <Text fontSize='preLarge' fontWeight='bold' color='text.secondary'>
-        {project?.name}
+        {project?.projectName}
       </Text>
       <Layout flexBasis={30} flexShrink={0} />
       <Text fontSize='semiMedium' color='text.gray'>
