3828c782a1e86f4d99851b3b5f154fa3a8bef6b2
gfg7 <nakonechnaya040702@gmail.com>
2023-11-03 00:46:44 +0500

    add project count & participant list

---
+++ features/team/server/Garnet.Teams.Infrastructure/Api/TeamsListByUser/TeamByUserPayload.cs
@@ -0,0 +1,16 @@
+using Garnet.Teams.Infrastructure.Api.TeamGet;
+using Garnet.Teams.Infrastructure.Api.TeamParticipantSearch;
+
+namespace Garnet.Teams.Infrastructure.Api.TeamsListByUser
+{
+    public record TeamByUserPayload(
+       string Id,
+       string Name,
+       string Description,
+       string? AvatarUrl,
+       string[] Tags,
+       string OwnerUserId,
+       int ProjectCount,
+       TeamParticipantPayload[] TeamParticipants
+   ) : TeamPayload(Id, Name, Description, AvatarUrl, Tags, OwnerUserId);
+}
