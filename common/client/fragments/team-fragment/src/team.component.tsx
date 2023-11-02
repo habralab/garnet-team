ceef3ea012ff390b14ab24244e2369b90ed5a1c4
Dmitry <dimafomin02@gmail.com>
2023-11-02 14:48:42 +0300

    feat: added base get-teams query

--- common/client/fragments/team-fragment/src/team.component.tsx
+++ common/client/fragments/team-fragment/src/team.component.tsx
@@ -16,15 +16,17 @@ import { Tag }              from '@ui/tag'
 import { Text }             from '@ui/text'
 import { Title }            from '@ui/title'
 import { WrapperWhite }     from '@ui/wrapper'
-import { mockTeams }        from '@shared/data'
 import { getUniqueTags }    from '@shared/helpers'
+
+import { useGetTeams }      from './data'
 
 export const Team: FC = () => {
   const [selectedTags, setSelectedTags] = useState<string[]>([])
   const [searchValue, setSearchValue] = useState<string>('')
   const { formatMessage } = useIntl()
 
-  const teams = mockTeams
+  const { teams } = useGetTeams({ search: '', skip: 0, tags: [], take: 0 })
+
   const uniqueTags = getUniqueTags(teams)
 
   const handleAddTag = (tag: string) => {
@@ -35,6 +37,7 @@ export const Team: FC = () => {
     setSelectedTags(selectedTags.filter((item) => item !== tag))
   }
 
+  /** @todo search params to url query */
   const filteredTeams = teams.filter(
     (team) =>
       selectedTags.some((tag) => team.tags?.includes(tag)) ||
