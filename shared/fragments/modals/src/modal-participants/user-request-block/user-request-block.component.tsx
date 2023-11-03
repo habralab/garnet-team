6d5643bad12f5c4db1eaee38a69b51f587c87b62
Dmitry <dimafomin02@gmail.com>
2023-11-03 17:09:14 +0300

    feat: added decide team join mutation

--- shared/fragments/modals/src/modal-participants/user-request-block/user-request-block.component.tsx
+++ shared/fragments/modals/src/modal-participants/user-request-block/user-request-block.component.tsx
@@ -1,28 +1,39 @@
-import React                     from 'react'
-import { FC }                    from 'react'
-import { FormattedDate }         from 'react-intl'
-import { FormattedRelativeTime } from 'react-intl'
-import { FormattedMessage }      from 'react-intl'
+import React                        from 'react'
+import { FC }                       from 'react'
+import { FormattedDate }            from 'react-intl'
+import { FormattedRelativeTime }    from 'react-intl'
+import { FormattedMessage }         from 'react-intl'
 
-import { Avatar }                from '@ui/avatar'
-import { Button }                from '@ui/button'
-import { Condition }             from '@ui/condition'
-import { HourglassIcon }         from '@ui/icon'
-import { CheckIcon }             from '@ui/icon'
-import { Box }                   from '@ui/layout'
-import { Column }                from '@ui/layout'
-import { Row }                   from '@ui/layout'
-import { Layout }                from '@ui/layout'
-import { Tag }                   from '@ui/tag'
-import { Text }                  from '@ui/text'
+import { Avatar }                   from '@ui/avatar'
+import { Button }                   from '@ui/button'
+import { Condition }                from '@ui/condition'
+import { HourglassIcon }            from '@ui/icon'
+import { CheckIcon }                from '@ui/icon'
+import { Box }                      from '@ui/layout'
+import { Column }                   from '@ui/layout'
+import { Row }                      from '@ui/layout'
+import { Layout }                   from '@ui/layout'
+import { Tag }                      from '@ui/tag'
+import { Text }                     from '@ui/text'
 
-import { UserRequestBlockProps } from './user-request-block.interfaces'
-import { getDaysAgo }            from './helpers'
+import { UserRequestBlockProps }    from './user-request-block.interfaces'
+import { useDecideTeamJoinRequest } from '../data'
+import { getDaysAgo }               from './helpers'
 
 export const UserRequestBlock: FC<UserRequestBlockProps> = ({ user, size = 'normal' }) => {
   const { id, avatarUrl, tags = [], userName, requestType, date = '' } = user
 
+  const { decideTeamJoinRequest } = useDecideTeamJoinRequest()
+
   const daysAgo = getDaysAgo(date)
+
+  const handleDecideTeamJoinRequest = (approve: boolean) => async () => {
+    try {
+      await decideTeamJoinRequest({ variables: { approve, id: user.requestId } })
+    } catch (error) {
+      /** @todo error notification */
+    }
+  }
 
   return (
     <Row>
@@ -54,7 +65,7 @@ export const UserRequestBlock: FC<UserRequestBlockProps> = ({ user, size = 'norm
         </Condition>
         <Layout flexBasis={8} flexShrink={0} />
         <Box>
-          <Button variant='link' size='micro'>
+          <Button variant='link' size='micro' onClick={handleDecideTeamJoinRequest(false)}>
             <Text fontSize='normal' color='currentColor'>
               <FormattedMessage
                 id={requestType === 'invite' ? 'profile.reject' : 'profile.cancel'}
@@ -77,7 +88,7 @@ export const UserRequestBlock: FC<UserRequestBlockProps> = ({ user, size = 'norm
       <Layout flexBasis={16} flexShrink={0} />
       <Box alignItems='center' width={size === 'large' ? 120 : 32}>
         <Condition match={requestType === 'application'}>
-          <Button variant='link' size='micro'>
+          <Button variant='link' size='micro' onClick={handleDecideTeamJoinRequest(true)}>
             <CheckIcon width={32} height={32} />
           </Button>
         </Condition>
