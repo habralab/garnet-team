e87ed2e794ad63cb839d356b86510670eaa7c787
picolino <webmaster.pico@gmail.com>
2023-11-03 18:09:03 +0300

    #2: Setup kratos deployment and connect user registration + login

--- features/user/server/Garnet.Users.Infrastructure/Api/Controllers/UserCreatedController.cs
+++ features/user/server/Garnet.Users.Infrastructure/Api/Controllers/UserCreatedController.cs
@@ -1,10 +1,15 @@
+using System.Threading;
+using System.Threading.Tasks;
+using Garnet.Common.Infrastructure.Identity;
 using Garnet.Users.Application.Commands;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Garnet.Users.Infrastructure.Api.Controllers
 {
     [ApiController]
     [Route("api/v1")]
+    [Authorize(AuthenticationSchemes = AuthSchemas.SecretKey)]
     public class UserCreatedController : ControllerBase
     {
         private readonly UserCreateCommand _userCreateCommand;
@@ -15,9 +20,9 @@ namespace Garnet.Users.Infrastructure.Api.Controllers
 
         [HttpPost]
         [Route("auth/registration/callback")]
-        public async Task UserCreated(UserCreated user)
+        public async Task UserCreated(CancellationToken ct, UserCreated user)
         {
-            await _userCreateCommand.Execute(CancellationToken.None, user.IdentityId, user.Username);
+            await _userCreateCommand.Execute(ct, user.IdentityId, user.Username);
         }
     }
 }
