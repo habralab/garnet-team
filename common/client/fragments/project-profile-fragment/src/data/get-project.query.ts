418a4614e84babb0dae5b0ddd6b30739e9774687
Dmitry <dimafomin02@gmail.com>
2023-11-03 03:50:27 +0300

    feat: added get project profile query

---
+++ common/client/fragments/project-profile-fragment/src/data/get-project.query.ts
@@ -0,0 +1,19 @@
+import { gql } from '@apollo/client'
+
+export const GET_PROJECT = gql`
+  query ProjectGet($id: String!) {
+    projectGet(projectId: $id) {
+      id
+      projectName
+      description
+      avatarUrl
+      tags
+      ownerUserId
+    }
+    projectTeamParticipantsFilter(input: { projectId: $id }) {
+      projectTeamParticipant {
+        id
+      }
+    }
+  }
+`
--- common/client/fragments/project-profile-fragment/src/project-profile.component.tsx
+++ common/client/fragments/project-profile-fragment/src/project-profile.component.tsx
@@ -5,6 +5,7 @@ import { useRouter }          from 'next/router'
 import { useEffect }          from 'react'
 import { useState }           from 'react'
 
+import { Project }            from '@shared/data'
 import { Condition }          from '@ui/condition'
 import { Box }                from '@ui/layout'
 import { Column }             from '@ui/layout'
@@ -14,24 +15,34 @@ import { Text }               from '@ui/text'
 import { Title }              from '@ui/title'
 import { WrapperWhite }       from '@ui/wrapper'
 import { mockAuthUserId }     from '@shared/data'
-import { getMockProject }     from '@shared/data'
-import { getMockUser }        from '@shared/data'
+import { useGetUser }         from '@shared/data'
 
 import { ButtonEditProject }  from './button-edit-project'
 import { ProfileAvatar }      from './profile-avatar'
 import { ProfileDescription } from './profile-description'
 import { ProfileTeams }       from './profile-teams'
+import { useGetProject }      from './data'
 
 export const ProjectProfile: FC = () => {
   const router = useRouter()
+  const queryId = typeof router.query.id === 'string' ? router.query.id : ''
+
+  const [project, setProject] = useState<Project>()
+
+  const { projectTeams, project: fetchedProject } = useGetProject({ id: queryId })
+  const { user: ownerUser } = useGetUser({ id: project?.ownerUserId || '', skip: 0, take: 20 })
+
   const [isMyProject, setIsMyProject] = useState(false)
 
-  const { project, projectTeams } = getMockProject(String(router.query.id) || '')
-  const ownerUser = getMockUser(project?.ownerUserId || '')?.user
-
   useEffect(() => {
-    if (project?.ownerUserId === mockAuthUserId) setIsMyProject(true)
-  }, [project])
+    if (fetchedProject) {
+      if (fetchedProject.ownerUserId === mockAuthUserId) setIsMyProject(true)
+
+      setProject(fetchedProject)
+    }
+  }, [router, fetchedProject])
+
+  const handleEditProject = (editedProject: Project) => setProject(editedProject)
 
   return (
     <Column fill>
@@ -51,7 +62,7 @@ export const ProjectProfile: FC = () => {
             <ProfileDescription project={project} />
             <Box position='absolute' bottom={32} right={32}>
               <Condition match={isMyProject}>
-                <ButtonEditProject project={project} />
+                <ButtonEditProject project={project} onEditProject={handleEditProject} />
               </Condition>
             </Box>
           </Condition>
