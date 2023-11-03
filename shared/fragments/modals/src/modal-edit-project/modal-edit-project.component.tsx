f7271d19c5609632675853392e73d51905578a41
Dmitry <dimafomin02@gmail.com>
2023-11-03 03:48:34 +0300

    feat: added edit project mutations

--- shared/fragments/modals/src/modal-edit-project/modal-edit-project.component.tsx
+++ shared/fragments/modals/src/modal-edit-project/modal-edit-project.component.tsx
@@ -1,24 +1,31 @@
-import React                     from 'react'
-import { FC }                    from 'react'
-import { useState }              from 'react'
-import { useIntl }               from 'react-intl'
+import React                      from 'react'
+import { FC }                     from 'react'
+import { useState }               from 'react'
+import { useIntl }                from 'react-intl'
 
-import { FormProject }           from '@shared/forms-fragment'
-import { FormProjectValues }     from '@shared/forms-fragment'
-import { Layout }                from '@ui/layout'
-import { Modal }                 from '@ui/modal'
+import { Project }                from '@shared/data'
+import { FormProject }            from '@shared/forms-fragment'
+import { FormProjectValues }      from '@shared/forms-fragment'
+import { Layout }                 from '@ui/layout'
+import { Modal }                  from '@ui/modal'
 
-import { ModalEditProjectProps } from './modal-edit-project.interfaces'
-import { getFormValues }         from '../helpers'
+import { ModalEditProjectProps }  from './modal-edit-project.interfaces'
+import { getFormValues }          from '../helpers'
+import { useUpdateProject }       from './data'
+import { useUploadProjectAvatar } from './data'
 
 export const ModalEditProject: FC<ModalEditProjectProps> = ({
   modalOpen = false,
   onClose,
+  onSubmit,
   project,
 }) => {
   const [formValues, setFormValues] = useState<FormProjectValues>(getFormValues(project))
   const [submitDisabled, setSubmitDisabled] = useState<boolean>(true)
   const { formatMessage } = useIntl()
+
+  const { updateProject } = useUpdateProject()
+  const { uploadProjectAvatar } = useUploadProjectAvatar()
 
   const closeModal = () => {
     setFormValues(getFormValues(project))
@@ -29,9 +36,29 @@ export const ModalEditProject: FC<ModalEditProjectProps> = ({
     setFormValues({ ...formValues, [field]: value })
   }
 
-  const handleSubmit = () => {
-    /** @todo submit form */
-    closeModal?.()
+  const handleSubmit = async () => {
+    try {
+      const { avatar, description, name, tags } = formValues
+
+      if (project?.avatarUrl !== avatar) {
+        const avatarFile = await fetch(avatar).then((r) => r.blob())
+
+        await uploadProjectAvatar({ variables: { id: project?.id, file: avatarFile } })
+      }
+
+      await updateProject({ variables: { id: project?.id, name, description, tags } })
+
+      onSubmit?.({
+        ...project,
+        projectName: name,
+        description,
+        tags,
+        avatarUrl: avatar,
+      })
+      closeModal?.()
+    } catch (error) {
+      /** @todo error notification */
+    }
   }
 
   return (
