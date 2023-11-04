f5d793e15f4f76d5b9601f8baae36a5f5cddd854
Demorey <demorey2@gmail.com>
2023-11-05 00:28:48 +0300

    #220 add TeamParticipantLeftTeamEvent consumer

---
+++ features/project/server/Garnet.Projects.Infrastructure/EventHandlers/User/TeamParticipantLeftTeamEventConsumer.cs
@@ -0,0 +1,25 @@
+﻿using Garnet.Common.Application.MessageBus;
+using Garnet.Projects.Application.ProjectTeam.Commands;
+using Garnet.Projects.Application.ProjectTeamParticipant.Commands;
+using Garnet.Teams.Events.TeamParticipant;
+
+namespace Garnet.Projects.Infrastructure.EventHandlers.User;
+
+public class TeamParticipantLeftTeamEventConsumer : IMessageBusConsumer<TeamParticipantLeftTeamEvent>
+{
+    private readonly ProjectTeamDeleteUserParticipantCommand _projectTeamDeleteUserParticipantCommand;
+    private readonly ProjectTeamParticipantDeleteUserParticipantCommand _projectTeamParticipantDeleteUserParticipantCommand;
+
+    public TeamParticipantLeftTeamEventConsumer(ProjectTeamDeleteUserParticipantCommand projectTeamDeleteUserParticipantCommand, ProjectTeamParticipantDeleteUserParticipantCommand projectTeamParticipantDeleteUserParticipantCommand)
+    {
+        _projectTeamDeleteUserParticipantCommand = projectTeamDeleteUserParticipantCommand;
+        _projectTeamParticipantDeleteUserParticipantCommand = projectTeamParticipantDeleteUserParticipantCommand;
+    }
+
+
+    public async Task Consume(TeamParticipantLeftTeamEvent message)
+    {
+        await _projectTeamDeleteUserParticipantCommand.Execute(CancellationToken.None, message.TeamId, message.UserId);
+        await _projectTeamParticipantDeleteUserParticipantCommand.Execute(CancellationToken.None, message.TeamId, message.UserId);
+    }
+}
