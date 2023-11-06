23d5471e9a230af2ef61347b4ce8470d886b9726
Dmitry <dimafomin02@gmail.com>
2023-11-06 14:50:34 +0300

    refactor: added use-skills-state hook

--- common/client/fragments/onboarding-fragment/src/enter-skills/enter-skills.component.tsx
+++ common/client/fragments/onboarding-fragment/src/enter-skills/enter-skills.component.tsx
@@ -11,28 +11,19 @@ import { Layout }           from '@ui/layout'
 import { Text }             from '@ui/text'
 
 import { EnterSkillsProps } from './enter-skills.interfaces'
-import { sizes }            from './enter-skills.config'
 import { validateValue }    from './helpers'
+import { useSkillsState }   from './hooks'
 
 export const EnterSkills: FC<EnterSkillsProps> = ({ onSubmit }) => {
-  const [value, setValue] = useState<string[]>([])
   const [errorText, setErrorText] = useState<string>('')
 
+  const { value, setValue, disabled } = useSkillsState(setErrorText)
+
   const { formatMessage } = useIntl()
-
-  const handleChange = (newValue: string | string[]) => {
-    if (Array.isArray(newValue)) setValue(newValue)
-
-    if (value.length >= sizes.min && value.length < sizes.max) {
-      setErrorText('')
-    }
-  }
 
   const updateErrorText = () => {
     validateValue(value, (id, values) => setErrorText(id ? formatMessage({ id }, values) : id))
   }
-
-  const isSubmitDisabled = value.length < sizes.min || value.length > sizes.max
 
   const handleSubmit = () => onSubmit?.(value)
 
@@ -48,14 +39,14 @@ export const EnterSkills: FC<EnterSkillsProps> = ({ onSubmit }) => {
         <Multiselect
           value={value}
           placeholder={formatMessage({ id: 'onboarding.enter_skill' })}
-          onChange={handleChange}
+          onChange={setValue}
           onBlur={updateErrorText}
           errorText={errorText}
         />
       </Box>
       <Layout flexBasis={40} flexShrink={0} />
       <Box justifyContent='flex-end'>
-        <Button variant='primary' size='normal' disabled={isSubmitDisabled} onClick={handleSubmit}>
+        <Button variant='primary' size='normal' disabled={disabled} onClick={handleSubmit}>
           <Text fontSize='medium' color='currentColor'>
             <FormattedMessage id='onboarding.save' />
           </Text>
