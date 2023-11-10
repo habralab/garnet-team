585614b01ed992760b1d932bc8c3b302c946fd0a
Nikita Elfimov <nelfimov@mail.ru>
2023-11-10 16:34:49 +0300

    refactor: naming

--- common/client/pages/onboard-page/src/onboard.page.tsx
+++ common/client/pages/onboard-page/src/onboard.page.tsx
@@ -1,14 +1,14 @@
 import React          from 'react'
 
 import { Header }     from '@app/header-fragment'
-import { Onboarding } from '@app/onboarding-fragment'
+import { OnBoarding } from '@app/onboard-fragment'
 import { Background } from '@ui/background'
 
-const UserPage = () => (
+const OnBoardPage = () => (
   <Background fill color='white' flexDirection='column' alignItems='center'>
     <Header disableNavigation />
-    <Onboarding />
+    <OnBoarding />
   </Background>
 )
 
-export default UserPage
+export default OnBoardPage
