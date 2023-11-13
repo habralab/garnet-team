0b80e98b1a0ec5fed22a164becfcd37ce279e513
Dmitry <dimafomin02@gmail.com>
2023-11-13 16:28:45 +0300

    feat: added shared routes

---
+++ shared/routes/src/routes.ts
@@ -0,0 +1,11 @@
+export const routes = {
+  root: '/',
+  onboard: '/onboard',
+  profile: '/profile',
+  projects: '/projects',
+  teams: '/teams',
+  teamsInvite: '/teams/invite',
+  users: '/users',
+  usersProjects: '/users/projects',
+  usersTeams: '/users/teams',
+}
