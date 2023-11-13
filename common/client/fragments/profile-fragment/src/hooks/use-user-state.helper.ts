14ceb71f8b1a10494071106a49f1d5a8901f1963
Nikita Elfimov <nelfimov@mail.ru>
2023-11-13 08:45:12 +0300

    fix: condition of a not filled profile

--- common/client/fragments/profile-fragment/src/hooks/use-user-state.helper.ts
+++ common/client/fragments/profile-fragment/src/hooks/use-user-state.helper.ts
@@ -3,5 +3,5 @@ import { IsProfileFilled } from './use-user-state.interfaces'
 export const isProfileNotFilled: IsProfileFilled = (user) => {
   if (!user) return true
 
-  return !user.avatarUrl || !user.description || !user.tags || !(user.tags.length === 0)
+  return !user.avatarUrl || !user.description || !user.tags || user.tags.length === 0
 }
