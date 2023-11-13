368788587c980d0fc87b3e8fc043507a46376cfb
Nikita Elfimov <nelfimov@mail.ru>
2023-11-13 08:46:42 +0300

    feat: add explicist interface

--- common/client/fragments/profile-fragment/src/hooks/use-user-state.interfaces.ts
+++ common/client/fragments/profile-fragment/src/hooks/use-user-state.interfaces.ts
@@ -1,3 +1,16 @@
-import { User } from '@shared/data'
+import { SetStateAction } from 'react'
+import { Dispatch }       from 'react'
+
+import { Team }           from '@shared/data'
+import { Project }        from '@shared/data'
+import { User }           from '@shared/data'
 
 export type IsProfileFilled = (user: User | undefined) => boolean
+
+export type UseProfileStateProps = () => {
+  projects: Project[]
+  teams: Team[]
+  setUser: Dispatch<SetStateAction<User | undefined>>
+  isNotFilled: boolean
+  user: User | undefined
+}
