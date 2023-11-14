6c2db9d4fd638f05ae54db6b6f31c032d09e1b39
Demorey <demorey2@gmail.com>
2023-11-14 22:59:37 +0300

    #248 add rating calculation to command

--- features/project/server/Garnet.Projects.Application/ProjectTask/Commands/ProjectTaskCloseCommand.cs
+++ features/project/server/Garnet.Projects.Application/ProjectTask/Commands/ProjectTaskCloseCommand.cs
@@ -5,6 +5,8 @@ using Garnet.Projects.Application.Project;
 using Garnet.Projects.Application.Project.Errors;
 using Garnet.Projects.Application.ProjectTask.Args;
 using Garnet.Projects.Application.ProjectTask.Errors;
+using Garnet.Projects.Application.ProjectTeamParticipant;
+using Garnet.Projects.Events.ProjectTask;
 
 namespace Garnet.Projects.Application.ProjectTask.Commands;
 
@@ -14,17 +16,20 @@ public class ProjectTaskCloseCommand
     private readonly IProjectTaskRepository _projectTaskRepository;
     private readonly IProjectRepository _projectRepository;
     private readonly IMessageBus _messageBus;
+    private readonly IProjectTeamParticipantRepository _projectTeamParticipantRepository;
 
     public ProjectTaskCloseCommand(
         ICurrentUserProvider currentUserProvider,
         IProjectTaskRepository projectTaskRepository,
         IMessageBus messageBus,
-        IProjectRepository projectRepository)
+        IProjectRepository projectRepository,
+        IProjectTeamParticipantRepository projectTeamParticipantRepository)
     {
         _currentUserProvider = currentUserProvider;
         _projectTaskRepository = projectTaskRepository;
         _messageBus = messageBus;
         _projectRepository = projectRepository;
+        _projectTeamParticipantRepository = projectTeamParticipantRepository;
     }
 
     public async Task<Result<ProjectTaskEntity>> Execute(CancellationToken ct, string taskId)
@@ -53,9 +58,39 @@ public class ProjectTaskCloseCommand
             return Result.Fail(new ProjectTaskAlreadySetThisStatusError(task.Status));
         }
 
+        RatingCalculation? ratingCalculation = null;
+
+        if (task.Reopened is false)
+        {
+            var skillScore = (float)Math.Round(9.5 / task.UserExecutorIds.Length * 0.8 / 3, 2,
+                MidpointRounding.AwayFromZero);
+            var teamScorePerUser = (float)Math.Round(9.5 / task.UserExecutorIds.Length * 0.2 / 3, 2,
+                MidpointRounding.AwayFromZero);
+
+            var tags = task.Tags;
+            var skillScorePerUser = tags.ToDictionary(x => x, x => skillScore);
+
+            var teams = await _projectTeamParticipantRepository.GetProjectTeamParticipantsByProjectId(ct, project.Id);
+            var teamExecutors = teams.Where(x => task.TeamExecutorIds.Contains(x.Id)).ToList();
+
+            Dictionary<string, float> teamsScore = new();
+
+            foreach (var team in teamExecutors)
+            {
+                var users = team.UserParticipants.Where(x => task.UserExecutorIds.Contains(x.Id)).ToList();
+                teamsScore[team.TeamId] = users.Count * teamScorePerUser;
+            }
+
+            ratingCalculation = new RatingCalculation(
+                (float)0.5,
+                task.UserExecutorIds,
+                skillScorePerUser,
+                teamsScore);
+        }
+
         task = await _projectTaskRepository.CloseProjectTask(ct, taskId, ProjectTaskStatuses.Close);
 
-        await _messageBus.Publish(task.ToClosedEvent());
+        await _messageBus.Publish(task.ToClosedEvent() with { RatingCalculation = ratingCalculation });
         return Result.Ok(task);
     }
 }
