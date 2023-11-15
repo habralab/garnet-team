5dc04637febe306453beb8cc4d24203c6a5955bd
Dmitry <dimafomin02@gmail.com>
2023-11-15 14:11:08 +0300

    fix: removed mock data

--- shared/data/src/get-user/use-get-user.hook.ts
+++ shared/data/src/get-user/use-get-user.hook.ts
@@ -3,7 +3,6 @@ import { useQuery }        from '@apollo/client'
 import { FilterVariables } from '@shared/data'
 import { Team }            from '@shared/data'
 import { User }            from '@shared/data'
-import { mockMyUser }      from '@shared/data'
 
 import { Project }         from '../data.interfaces'
 import { GET_USER }        from './get-user.query'
@@ -26,15 +25,6 @@ export const useGetUser = ({ id, search = '', skip = 0, tags = [], take = 0 }: G
   const { data, refetch } = useQuery<GetUserResponse, GetUserInput>(GET_USER, {
     variables: { id, search, skip, tags, take },
   })
-
-  if (!data) {
-    return {
-      user: mockMyUser.userGet,
-      teams: mockMyUser?.teamsListByUser?.teams || [],
-      projects: mockMyUser?.projectsListByUser?.projects || [],
-    }
-  }
-
 
   return {
     user: data?.userGet,
