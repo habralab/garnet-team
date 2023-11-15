3b495c419236bb9a19e84655bb9b475eb04320a6
Dmitry <dimafomin02@gmail.com>
2023-11-15 05:25:31 +0300

    feat: added get-server-side props for users

--- common/client/entrypoints/renderer/src/pages/users/[id]/index.ts
+++ common/client/entrypoints/renderer/src/pages/users/[id]/index.ts
@@ -1,1 +1,1 @@
-export { default } from '@app/user-page'
+export { default, getServerSideProps } from '@app/user-page'
--- shared/data/src/get-user/use-get-user.hook.ts
+++ shared/data/src/get-user/use-get-user.hook.ts
@@ -3,6 +3,7 @@ import { useQuery }        from '@apollo/client'
 import { FilterVariables } from '@shared/data'
 import { Team }            from '@shared/data'
 import { User }            from '@shared/data'
+import { mockMyUser }      from '@shared/data'
 
 import { Project }         from '../data.interfaces'
 import { GET_USER }        from './get-user.query'
@@ -25,6 +26,12 @@ export const useGetUser = ({ id, search = '', skip = 0, tags = [], take = 0 }: G
   const { data, refetch } = useQuery<GetUserResponse, GetUserInput>(GET_USER, {
     variables: { id, search, skip, tags, take },
   })
+
+  return {
+    user: mockMyUser.userGet,
+    teams: mockMyUser?.teamsListByUser?.teams || [],
+    projects: mockMyUser?.projectsListByUser?.projects || [],
+  }
 
   return {
     user: data?.userGet,
