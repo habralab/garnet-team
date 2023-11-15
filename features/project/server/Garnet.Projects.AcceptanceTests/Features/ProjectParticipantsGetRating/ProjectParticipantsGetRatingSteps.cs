d62e8efe9967c21e03c4e671fe272b209c7759dc
Demorey <demorey2@gmail.com>
2023-11-15 23:50:36 +0300

    #248 add test steps

--- features/project/server/Garnet.Projects.AcceptanceTests/Features/ProjectParticipantsGetRating/ProjectParticipantsGetRatingSteps.cs
+++ features/project/server/Garnet.Projects.AcceptanceTests/Features/ProjectParticipantsGetRating/ProjectParticipantsGetRatingSteps.cs
@@ -1,5 +1,6 @@
 ﻿using FluentAssertions;
 using Garnet.Common.AcceptanceTests.Fakes;
+using Garnet.Projects.AcceptanceTests.FakeServices;
 using Garnet.Projects.AcceptanceTests.Support;
 using Garnet.Projects.Application.ProjectTask.Args;
 using Garnet.Projects.Infrastructure.MongoDb.ProjectTask;
@@ -17,9 +18,15 @@ public class ProjectParticipantsGetRatingSteps : BaseSteps
     private readonly UpdateDefinitionBuilder<ProjectTaskDocument> _u =
         Builders<ProjectTaskDocument>.Update;
 
-    public ProjectParticipantsGetRatingSteps(StepsArgs args) :
+    private readonly ProjectTaskClosedEventFakeConsumer _taskClosedFake;
+    private readonly CurrentUserProviderFake _currentUserProviderFake;
+
+    public ProjectParticipantsGetRatingSteps(StepsArgs args, ProjectTaskClosedEventFakeConsumer taskClosedFake,
+        CurrentUserProviderFake currentUserProviderFake) :
         base(args)
     {
+        _taskClosedFake = taskClosedFake;
+        _currentUserProviderFake = currentUserProviderFake;
     }
 
     [Given(@"задача '([^']*)' повторно открыта")]
@@ -40,10 +47,53 @@ public class ProjectParticipantsGetRatingSteps : BaseSteps
         await Db.ProjectTasks.InsertOneAsync(task);
     }
 
+    [Given(@"пользователь '(.*)' является исполнителем задачи '(.*)'")]
+    public async Task GivenПользовательЯвляетсяИсполнителемЗадачи(string username, string taskName)
+    {
+        var user = await Db.ProjectUsers.Find(x => x.UserName == username).FirstAsync();
+        await Db.ProjectTasks.UpdateOneAsync(
+            _f.Eq(x => x.Name, taskName),
+            _u.AddToSet(x => x.UserExecutorIds, user.Id));
+    }
+
     [Then(@"в системе существует задача с названием '([^']*)' и статусом '([^']*)'")]
     public async Task ThenВСистемеСуществуетЗадачаСоСтатусом(string taskName, string status)
     {
         var task = await Db.ProjectTasks.Find(x => x.Name == taskName).FirstOrDefaultAsync();
         task.Status.Should().Be(status);
+    }
+
+    [Then(
+        @"у пользователя '([^']*)' общий рейтинг равен '([^']*)' а рейтинг каждого из навыков '([^']*)' равен '([^']*)'")]
+    public void ThenУПользователяОбщийРейтингИРейтингНавыков(string username, float totalScore, string tags,
+        float skillScore)
+    {
+        var tagList = tags.Split(", ");
+
+        _taskClosedFake.GetMessage().RatingCalculation!.UserExecutorIds
+            .Contains(_currentUserProviderFake.GetUserIdByUsername(username)).Should().BeTrue();
+
+        _taskClosedFake.GetMessage().RatingCalculation!.UserTotalScore.Should().Be(totalScore);
+
+        var skills = _taskClosedFake.GetMessage().RatingCalculation!.SkillScorePerUser;
+        foreach (var tag in tagList)
+        {
+            skills[tag].Should().Be(skillScore);
+        }
+    }
+
+    [Then(@"у команды '(.*)' общий рейтинг равен '(.*)'")]
+    public async void ThenУКомандыОбщийРейтинг(string teamName, float totalScore)
+    {
+        var team = await Db.ProjectTeams.Find(x => x.TeamName == teamName).FirstAsync();
+        _taskClosedFake.GetMessage().RatingCalculation!.TeamsTotalScore[team.Id].Should().Be(totalScore);
+    }
+
+    [Then(@"у пользователя '([^']*)' общий рейтинг равен '([^']*)'")]
+    public void ThenУПользователяОбщийРейтинг(string username, float totalScore)
+    {
+        _taskClosedFake.GetMessage().RatingCalculation!.ProjectOwnerId.Should()
+            .Be(_currentUserProviderFake.GetUserIdByUsername(username));
+        _taskClosedFake.GetMessage().RatingCalculation!.ProjectOwnerTotalScore.Should().Be(totalScore);
     }
 }
