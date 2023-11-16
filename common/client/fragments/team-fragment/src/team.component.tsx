17d99a0bf737cf2963d5ddb525db8b0cd3f4dcab
Dmitry <dimafomin02@gmail.com>
2023-11-16 16:23:50 +0300

    feat: added server-side filtering teams/projects

--- common/client/fragments/team-fragment/src/team.component.tsx
+++ common/client/fragments/team-fragment/src/team.component.tsx
@@ -1,6 +1,8 @@
 import React                from 'react'
 import { FC }               from 'react'
 import { FormattedMessage } from 'react-intl'
+import { useEffect }        from 'react'
+import { useMemo }          from 'react'
 import { useState }         from 'react'
 import { useIntl }          from 'react-intl'
 
@@ -17,18 +19,31 @@ import { Text }             from '@ui/text'
 import { Title }            from '@ui/title'
 import { WrapperWhite }     from '@ui/wrapper'
 import { getUniqueTags }    from '@shared/utils'
+import { useDebounce }      from '@shared/utils'
 
 import { useGetTeams }      from './data'
-import { filterTeams }      from './helpers'
 
 export const Team: FC = () => {
+  const [uniqueTags, setUniqueTags] = useState<string[]>([])
   const [selectedTags, setSelectedTags] = useState<string[]>([])
   const [searchValue, setSearchValue] = useState<string>('')
   const { formatMessage } = useIntl()
 
-  const { teams } = useGetTeams({ search: '', skip: 0, tags: [], take: 0 })
+  const filters = useMemo(
+    () => ({ search: searchValue, tags: selectedTags, skip: 0, take: 0 }),
+    [searchValue, selectedTags]
+  )
+  const debounceFilters = useDebounce(filters)
 
-  const uniqueTags = getUniqueTags(teams)
+  const { teams } = useGetTeams(debounceFilters)
+
+  useEffect(() => {
+    if (teams.length > 0 && uniqueTags.length === 0) {
+      setUniqueTags(getUniqueTags(teams))
+    }
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [teams])
 
   const handleAddTag = (tag: string) => {
     setSelectedTags(selectedTags.concat([tag]))
@@ -37,9 +52,6 @@ export const Team: FC = () => {
   const handleRemoveTag = (tag: string) => () => {
     setSelectedTags(selectedTags.filter((item) => item !== tag))
   }
-
-  /** @todo search params to url query */
-  const filteredTeams = filterTeams(teams, selectedTags, searchValue)
 
   return (
     <Column fill marginBottom={32}>
@@ -75,7 +87,7 @@ export const Team: FC = () => {
               </Box>
             </Condition>
             <Condition match={teams.length > 0}>
-              <Condition match={filteredTeams.length === 0}>
+              <Condition match={teams.length === 0}>
                 <Layout flexBasis={60} flexShrink={0} />
                 <Box fill width={350} justifyContent='center' alignItems='center'>
                   <Text
@@ -90,14 +102,14 @@ export const Team: FC = () => {
                 </Box>
                 <Layout flexBasis={60} flexShrink={0} />
               </Condition>
-              <Condition match={filteredTeams.length > 0}>
+              <Condition match={teams.length > 0}>
                 <Text fontSize='regular' fontWeight='bold' color='text.secondary'>
                   <FormattedMessage id='team.teams_found' />
-                  {`: ${filteredTeams.length}`}
+                  {`: ${teams.length}`}
                 </Text>
                 <Layout flexBasis={42} flexShrink={0} />
                 <Grid gap={22} gridWrap='small' justifyContent='space-between'>
-                  {filteredTeams.map((team) => (
+                  {teams.map((team) => (
                     <CardTeam key={team.id} team={team} cardSize='small' />
                   ))}
                 </Grid>
