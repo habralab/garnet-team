f3fa13ef1c46adf6d640a407e3ebf98a13129f32
gfg7 <nakonechnaya040702@gmail.com>
2023-11-29 21:38:03 +0500

    #268 fix team edit owner

--- features/team/server/Garnet.Teams.Application/Team/Notifications/TeamEntityNotificationsExtensions.cs
+++ features/team/server/Garnet.Teams.Application/Team/Notifications/TeamEntityNotificationsExtensions.cs
@@ -1,18 +1,26 @@
 using Garnet.Notifications.Events;
+using Garnet.Teams.Application.TeamUser;
 
 namespace Garnet.Teams.Application.Team.Notifications
 {
     public static class TeamEntityNotificationsExtensions
     {
-        public static SendNotificationCommandMessage CreateTeamEditOwnerNotification(this TeamEntity team, string username)
+        public static SendNotificationCommandMessage CreateTeamEditOwnerNotification(this TeamEntity team, TeamUserEntity user)
         {
+            var quotes = new NotificationQuotedEntity[]
+            {
+                new(team.Id, team.AvatarUrl, team.Name),
+                new(user.Id, user.AvatarUrl, user.Username)
+            };
+
             return new SendNotificationCommandMessage(
                 Title: "Смена владельца команды",
-                Body: $"Владелец команды {team.Name} изменен на пользователя {username}",
+                Body: $"Владелец команды {team.Name} изменен на пользователя {user.Username}",
                 team.OwnerUserId,
                 Type: "TeamEditOwner",
                 team.AuditInfo.UpdatedAt,
-                team.Id
+                team.Id,
+                quotes
             );
         }
     }
