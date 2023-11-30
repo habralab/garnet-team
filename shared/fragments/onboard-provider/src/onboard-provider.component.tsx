86955c9b714f16757757ef16b54115babba1f179
Dmitry <dimafomin02@gmail.com>
2023-11-30 17:05:17 +0300

    fix: onboard rerenders

--- shared/fragments/onboard-provider/src/onboard-provider.component.tsx
+++ shared/fragments/onboard-provider/src/onboard-provider.component.tsx
@@ -2,6 +2,7 @@ import React                  from 'react'
 import { PropsWithChildren }  from 'react'
 import { FC }                 from 'react'
 import { useRouter }          from 'next/router'
+import { useRef }             from 'react'
 import { useEffect }          from 'react'
 
 import { useGetUser }         from '@shared/data'
@@ -13,12 +14,21 @@ import { isProfileNotFilled } from './onboard-provider.helper'
 export const OnboardProvider: FC<PropsWithChildren> = ({ children }) => {
   const { userId } = useSession()
 
+  const isAlreadyOnboardRef = useRef<boolean>(false)
+
   const { user } = useGetUser({ id: userId })
 
   const { push } = useRouter()
 
   useEffect(() => {
-    if (user && isProfileNotFilled(user)) push(routes.onboard)
+    if (user && !isAlreadyOnboardRef.current && isProfileNotFilled(user)) {
+      push(routes.onboard)
+      isAlreadyOnboardRef.current = true
+    }
+
+    if (user && user.id !== userId) {
+      isAlreadyOnboardRef.current = false
+    }
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [user, userId])
